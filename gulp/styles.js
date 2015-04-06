8ee9742db301238c6b6b85b5829bcdd9538243d4
Travis Tidwell <codertravist@gmail.com>
2015-04-05 23:03:59 -0500

    Updates to include the server with the app.

--- gulp/styles.js
+++ gulp/styles.js
@@ -1,6 +1,6 @@
 module.exports = function(gulp, plugins) {
   return function () {
-    return gulp.src('app/styles/main.scss')
+    return gulp.src('app/styles/**/*.scss')
       .pipe(plugins.sourcemaps.init())
       .pipe(plugins.sass({
         outputStyle: 'nested', // libsass doesn't support expanded yet
---
+++ server.js
@@ -0,0 +1,32 @@
+var formio = require('formio')(require('config'));
+var express = require('express');
+var nunjucks = require('nunjucks');
+var app = express();
+
+// Configure nunjucks.
+nunjucks.configure('server/views', {
+  express   : app
+});
+
+// Use swagger docs and assets.
+app.use('/', express.static(__dirname + '/dist'));
+app.use('/assets', express.static(__dirname + '/bower_components'));
+app.use('/swagger', express.static(require('swagger-ui').dist));
+
+// Show the docs page for the API.
+app.get('/spec.html', function(req, res, next) {
+  res.render('docs.html', {
+    url: req.protocol + '://' + req.get('host') + '/api/spec.json'
+  });
+});
+
+// Get the specs for each form.
+app.get('/form/:formId/spec.html', function(req, res, next) {
+  res.render('docs.html', {
+    url: req.protocol + '://' + req.get('host') + '/api/form/:formId/spec.json'
+  });
+});
+
+app.use('/api', formio);
+console.log('Listening to port 3000');
+app.listen(3000);
