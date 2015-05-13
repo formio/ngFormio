c5e856468b74beb2f1163ec4c630caad6ec15e5d
Rahat Ahmed <rahatarmanahmed@gmail.com>
2015-05-13 11:07:16 -0500

    Added radio component

--- dist/formio.js
+++ dist/formio.js
@@ -1460,6 +1460,63 @@ app.run([
 app.config([
   'formioComponentsProvider',
   function(formioComponentsProvider) {
+    formioComponentsProvider.register('radio', {
+      title: 'Radio',
+      template: 'formio/components/radio.html',
+      settings: {
+        input: true,
+        inputType: 'radio',
+        label: '',
+        key: '',
+        values: [
+          {
+            value: 'value1',
+            label: 'Value 1'
+          },
+          {
+            value: 'value2',
+            label: 'Value 2'
+          }
+        ],
+        defaultValue: '',
+        protected: false,
+        persistent: true,
+        validate: {
+          required: false,
+          custom: '',
+          customPrivate: false
+        }
+      }
+    });
+  }
+]);
+app.run([
+  '$templateCache',
+  'FormioUtils',
+  function(
+    $templateCache,
+    FormioUtils
+  ) {
+    $templateCache.put('formio/components/radio.html', FormioUtils.fieldWrap(
+      '<div class="radio" ng-repeat="v in component.values track by $index">' +
+        '<label class="control-label" for="value">' +
+          '<input type="{{ component.inputType }}" ' +
+            'id="{{ v.value }}" ' +
+            'name="{{ component.key }}" ' +
+            'value="{{ v.value }}" ' +
+            'ng-model="data[component.key]" ' +
+            'ng-required="component.validate.required" ' +
+            'custom-validator="component.validate.custom">' +
+          '{{ v.label }}' +
+        '</label>' +
+      '</div>'
+    ));
+  }
+]);
+
+app.config([
+  'formioComponentsProvider',
+  function(formioComponentsProvider) {
     formioComponentsProvider.register('resource', {
       title: 'Resource',
       template: function($scope) {
