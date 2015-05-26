87aec4bdbba2b3703da3ab7236d96a17be0279d2
Travis Tidwell <codertravist@gmail.com>
2015-05-26 10:23:31 -0500

    Adding table support.

--- components/table.js
+++ components/table.js
@@ -1,13 +1,19 @@
 app.config([
   'formioComponentsProvider',
   function(formioComponentsProvider) {
-    formioComponentsProvider.register('columns', {
-      title: 'Columns',
-      template: 'formio/components/columns.html',
+    formioComponentsProvider.register('table', {
+      title: 'Table',
+      template: 'formio/components/table.html',
       group: 'layout',
       settings: {
         input: false,
-        columns: [{components: []},{components: []}]
+        rows: [[{components: []},{components: []}], [{components: []},{components: []}]],
+        header: [],
+        caption: '',
+        striped: false,
+        bordered: false,
+        hover: false,
+        condensed: false
       }
     });
   }
@@ -15,11 +21,24 @@ app.config([
 app.run([
   '$templateCache',
   function($templateCache) {
-    $templateCache.put('formio/components/columns.html',
-      '<div class="row">' +
-        '<div class="col-xs-6" ng-repeat="column in component.columns">' +
-          '<formio-component ng-repeat="component in column.components" component="component" data="data" formio="formio"></formio-component>' +
-        '</div>' +
+    var tableClasses = "{'table-striped': component.striped, ";
+    tableClasses += "'table-bordered': component.bordered, ";
+    tableClasses += "'table-hover': component.hover, ";
+    tableClasses += "'table-condensed': component.condensed}";
+    $templateCache.put('formio/components/table.html',
+      '<div class="table-responsive">' +
+        '<table ng-class="' + tableClasses + '" class="table">' +
+          '<thead ng-if="component.header.length"><tr>' +
+            '<th ng-repeat="header in component.header">{{ header }}</th>' +
+          '</tr></thead>' +
+          '<tbody>' +
+            '<tr ng-repeat="row in component.rows">' +
+              '<td ng-repeat="column in row">' +
+                '<formio-component ng-repeat="component in column.components" component="component" data="data" formio="formio"></formio-component>' +
+              '</td>' +
+            '</tr>' +
+          '</tbody>' +
+        '</table>' +
       '</div>'
     );
   }
--- dist/formio.js
+++ dist/formio.js
@@ -1915,6 +1915,52 @@ app.run([
 app.config([
   'formioComponentsProvider',
   function(formioComponentsProvider) {
+    formioComponentsProvider.register('table', {
+      title: 'Table',
+      template: 'formio/components/table.html',
+      group: 'layout',
+      settings: {
+        input: false,
+        rows: [[{components: []},{components: []}], [{components: []},{components: []}]],
+        header: [],
+        caption: '',
+        striped: false,
+        bordered: false,
+        hover: false,
+        condensed: false
+      }
+    });
+  }
+]);
+app.run([
+  '$templateCache',
+  function($templateCache) {
+    var tableClasses = "{'table-striped': component.striped, ";
+    tableClasses += "'table-bordered': component.bordered, ";
+    tableClasses += "'table-hover': component.hover, ";
+    tableClasses += "'table-condensed': component.condensed}";
+    $templateCache.put('formio/components/table.html',
+      '<div class="table-responsive">' +
+        '<table ng-class="' + tableClasses + '" class="table">' +
+          '<thead ng-if="component.header.length"><tr>' +
+            '<th ng-repeat="header in component.header">{{ header }}</th>' +
+          '</tr></thead>' +
+          '<tbody>' +
+            '<tr ng-repeat="row in component.rows">' +
+              '<td ng-repeat="column in row">' +
+                '<formio-component ng-repeat="component in column.components" component="component" data="data" formio="formio"></formio-component>' +
+              '</td>' +
+            '</tr>' +
+          '</tbody>' +
+        '</table>' +
+      '</div>'
+    );
+  }
+]);
+
+app.config([
+  'formioComponentsProvider',
+  function(formioComponentsProvider) {
     formioComponentsProvider.register('textarea', {
       title: 'Text Area',
       template: 'formio/components/textarea.html',
