92262d8a8012098ed7a79a29058a75d656b3ac5e
Travis Tidwell <codertravist@gmail.com>
2015-06-10 08:50:20 -0500

    Updating the boardman app.

--- apps/boardman/app/scripts/app.js
+++ apps/boardman/app/scripts/app.js
@@ -8,168 +8,265 @@
  *
  * Main module of the application.
  */
-var formioBaseUrl = 'http://form.io/app/api';
+var formioBaseUrl = 'https://form.io/app/api';
+var boardmanAPI = 'https://boardman.form.io/app/api';
+
 angular
   .module('boardmanApp', [
     'formio',
-    'ui.router',
-    'ui.select',
-    'angularMoment',
-    'ngSanitize',
-    'bgf.paginateAnything',
-    'restangular',
-    'angularMoment'
+    'ui.router'
+  ])
+  .filter('ucfirst', [function() {
+    return function(input) {
+      return input.charAt(0).toUpperCase() + input.substring(1);
+    };
+  }])
+  .provider('Resource', [
+    '$stateProvider',
+    function(
+      $stateProvider
+    ) {
+      var resources = {};
+      return {
+        register: function(name) {
+          resources[name] = name;
+          var formName = name + 'Form';
+          $stateProvider
+            .state(name + 'Index', {
+              url: '/' + name,
+              templateUrl: 'views/resource/index.html',
+              controller: ['$scope', '$state', function ($scope, $state) {
+                $scope.resourceName = name;
+                $scope.$on('submissionView', function(event, submission) {
+                  $state.go(name + '.view', {id: submission._id});
+                });
+
+                $scope.$on('submissionEdit', function(event, submission) {
+                  $state.go(name + '.edit', {id: submission._id});
+                });
+
+                $scope.$on('submissionDelete', function(event, submission) {
+                  $state.go(name + '.delete', {id: submission._id});
+                });
+              }]
+            })
+            .state(name + 'Create', {
+              url: '/create/' + name,
+              templateUrl: 'views/resource/create.html',
+              controller: ['$scope', '$state', function ($scope, $state) {
+                $scope.resourceForm = $scope[formName];
+                $scope.$on('formSubmission', function(event, submission) {
+                  $state.go(name + '.view', {id: submission._id});
+                });
+              }]
+            })
+            .state(name, {
+              abstract: true,
+              url: '/' + name + '/:id',
+              templateUrl: 'views/resource.html',
+              controller: ['$scope', '$stateParams', function ($scope, $stateParams) {
+                $scope.resourceName = name;
+                $scope.resourceForm = $scope[formName];
+                $scope.resourceUrl = $scope.resourceForm + '/submission/' + $stateParams.id;
+              }]
+            })
+            .state(name + '.view', {
+              url: '/',
+              parent: name,
+              templateUrl: 'views/' + name + '/view.html',
+              controller: ['$scope', '$stateParams', 'Formio', function ($scope, $stateParams, Formio) {
+                $scope.resource = {};
+                (new Formio($scope.resourceUrl)).loadSubmission().then(function(submission) {
+                  $scope.resource = submission;
+                });
+              }]
+            })
+            .state(name + '.edit', {
+              url: '/edit',
+              parent: name,
+              templateUrl: 'views/resource/edit.html',
+              controller: ['$scope', '$state', function ($scope, $state) {
+                $scope.$on('formSubmission', function(event, submission) {
+                  $state.go(name + '.view', {id: submission._id});
+                });
+              }]
+            })
+            .state(name + '.delete', {
+              url: '/delete',
+              parent: name,
+              templateUrl: 'views/resource/delete.html',
+              controller: ['$scope', '$state', function ($scope, $state) {
+                $scope.$on('delete', function() {
+                  $state.go(name + 'Index');
+                });
+              }]
+            });
+        },
+        $get: function() {
+          return resources;
+        }
+      };
+    }
   ])
   .config([
-    '$httpProvider',
     'FormioProvider',
+    'ResourceProvider',
     '$stateProvider',
     '$urlRouterProvider',
-    '$sceDelegateProvider',
     function(
-      $httpProvider,
       FormioProvider,
+      ResourceProvider,
       $stateProvider,
       $urlRouterProvider
     ) {
 
-      // Add authorization.
-      $httpProvider.defaults.headers.common.Authorization = 'Basic ' + window.btoa(unescape(encodeURIComponent('formio:FoRmI@4ever!')));
-
       // Set the base url for formio.
       FormioProvider.setBaseUrl(formioBaseUrl);
 
+      // Add the home state.
       $stateProvider
         .state('home', {
           url: '/?',
           templateUrl: 'views/main.html',
-          controller: 'MainCtrl'
+          controller: [
+            '$scope',
+            '$rootScope',
+            function (
+              $scope,
+              $rootScope
+            ) {
+              $scope.quotes = [];
+              $scope.quotesUrl = $rootScope.quoteForm + '/submission';
+              $scope.customers = [];
+              $scope.customersUrl = $rootScope.customerForm + '/submission';
+              $scope.companies = [];
+              $scope.companiesUrl = $rootScope.companyForm + '/submission';
+            }
+          ]
         })
-        .state('survey', {
-          url: '/survey',
-          templateUrl: 'views/survey.html'
+        .state('auth', {
+          abstract: true,
+          url: '/auth',
+          templateUrl: 'views/user/auth.html'
         })
-        .state('createQuote', {
-          url: '/create/quote',
-          templateUrl: 'views/quote/create.html',
-          controller: 'QuoteCreateCtrl'
-        })
-        .state('viewQuote', {
-          url: '/quote/:quoteId',
-          templateUrl: 'views/quote/view.html',
-          controller: 'QuoteViewCtrl'
-        })
-        .state('editQuote', {
-          url: '/quote/:quoteId/edit',
-          templateUrl: 'views/quote/edit.html',
-          controller: 'QuoteEditCtrl'
-        })
-        .state('deleteQuote', {
-          url: '/quote/:quoteId/delete',
-          templateUrl: 'views/quote/delete.html',
-          controller: 'QuoteDeleteCtrl'
-        })
-        .state('listQuotes', {
-          url: '/quote',
-          templateUrl: 'views/quote/index.html',
-          controller: 'QuoteIndexCtrl'
-        })
-        .state('listCustomers', {
-          url: '/customer',
-          templateUrl: 'views/customer/index.html',
-          controller: 'CustomerIndexCtrl'
-        })
-        .state('createCustomer', {
-          url: '/create/customer',
-          templateUrl: 'views/customer/create.html',
-          controller: 'CustomerCreateCtrl'
-        })
-        .state('viewCustomer', {
-          url: '/customer/:customerId',
-          templateUrl: 'views/customer/view.html',
-          controller: 'CustomerViewCtrl'
-        })
-        .state('editCustomer', {
-          url: '/customer/:customerId/edit',
-          templateUrl: 'views/customer/edit.html',
-          controller: 'CustomerEditCtrl'
-        })
-        .state('deleteCustomer', {
-          url: '/customer/:customerId/delete',
-          templateUrl: 'views/customer/delete.html',
-          controller: 'CustomerDeleteCtrl'
-        })
-        .state('listCompanies', {
-          url: '/company',
-          templateUrl: 'views/company/index.html',
-          controller: 'CompanyIndexCtrl'
-        })
-        .state('createCompany', {
-          url: '/create/company',
-          templateUrl: 'views/company/create.html',
-          controller: 'CompanyCreateCtrl'
-        })
-        .state('viewCompany', {
-          url: '/company/:companyId',
-          templateUrl: 'views/company/view.html',
-          controller: 'CompanyViewCtrl'
-        })
-        .state('editCompany', {
-          url: '/company/:companyId/edit',
-          templateUrl: 'views/company/edit.html',
-          controller: 'CompanyEditCtrl'
-        })
-        .state('deleteCompany', {
-          url: '/company/:companyId/delete',
-          templateUrl: 'views/company/delete.html',
-          controller: 'CompanyDeleteCtrl'
-        })
-        .state('listAgents', {
-          url: '/agent',
-          templateUrl: 'views/agent/index.html',
-          controller: 'AgentIndexCtrl'
-        })
-        .state('createAgent', {
-          url: '/create/agent',
-          templateUrl: 'views/agent/create.html',
-          controller: 'AgentCreateCtrl'
-        })
-        .state('viewAgent', {
-          url: '/agent/:agentId',
-          templateUrl: 'views/agent/view.html',
-          controller: 'AgentViewCtrl'
-        })
-        .state('editAgent', {
-          url: '/agent/:agentId/edit',
-          templateUrl: 'views/agent/edit.html',
-          controller: 'AgentEditCtrl'
-        })
-        .state('deleteAgent', {
-          url: '/agent/:agentId/delete',
-          templateUrl: 'views/agent/delete.html',
-          controller: 'AgentDeleteCtrl'
+        .state('auth.login', {
+          url: '/login',
+          parent: 'auth',
+          templateUrl: 'views/user/login.html',
+          controller: ['$scope', '$state', '$rootScope', function($scope, $state, $rootScope) {
+            $scope.$on('formSubmission', function(err, submission) {
+              if (!submission) { return; }
+              $rootScope.user = submission;
+              $state.go('home');
+            });
+          }]
         });
+
+      // Register the resources.
+      ResourceProvider.register('quote');
+      ResourceProvider.register('customer');
+      ResourceProvider.register('company');
+      ResourceProvider.register('agent');
 
       $urlRouterProvider.otherwise('/');
     }
   ])
+  .factory('FormioAlerts', [
+    '$rootScope',
+    function (
+      $rootScope
+    ) {
+      var alerts = [];
+      return {
+        addAlert: function (alert) {
+          $rootScope.alerts.push(alert);
+          if (alert.element) {
+            angular.element('#form-group-' + alert.element).addClass('has-error');
+          }
+          else {
+            alerts.push(alert);
+          }
+        },
+        getAlerts: function () {
+          var tempAlerts = angular.copy(alerts);
+          alerts.length = 0;
+          alerts = [];
+          return tempAlerts;
+        },
+        onError: function showError(error) {
+          if (error.message) {
+            this.addAlert({
+              type: 'danger',
+              message: error.message,
+              element: error.path
+            });
+          }
+          else {
+            var errors = error.hasOwnProperty('errors') ? error.errors : error.data.errors;
+            angular.forEach(errors, showError.bind(this));
+          }
+        }
+      };
+    }
+  ])
   .run([
     '$rootScope',
+    '$state',
+    'Formio',
+    'FormioAlerts',
     function(
-      $rootScope
+      $rootScope,
+      $state,
+      Formio,
+      FormioAlerts
     ) {
 
       // Set the quote form.
       $rootScope.baseUrl = formioBaseUrl;
-      /*
-      $rootScope.quoteForm = '/app/5524894395c16ae3dc0d8bef/resource/55269d9964b8770c138eb7ed';
-      $rootScope.customerForm = '/app/5524894395c16ae3dc0d8bef/resource/55269e2b64b8770c138eb7ef';
-      $rootScope.companyForm = '/app/5524894395c16ae3dc0d8bef/resource/55269e5264b8770c138eb7f0';
-      $rootScope.agentForm = '/app/5524894395c16ae3dc0d8bef/resource/55312198128b5a6b1d02640f';
-      */
-      $rootScope.quoteForm = '/app/552b2297d70ef854300001e5/resource/552b25eed7fae6f63c150e58';
-      $rootScope.customerForm = '/app/552b2297d70ef854300001e5/resource/552b22b3d70ef854300001e6';
-      $rootScope.companyForm = '/app/552b2297d70ef854300001e5/resource/552b258fd7fae6f63c150e57';
-      $rootScope.agentForm = '/app/552b2297d70ef854300001e5/resource/552b2622d7fae6f63c150e59';
+      $rootScope.quoteForm = boardmanAPI + '/quote';
+      $rootScope.customerForm = boardmanAPI + '/customer';
+      $rootScope.companyForm = boardmanAPI + '/company';
+      $rootScope.userLoginForm = boardmanAPI + '/agent/login';
+
+      // Set the user who is logged in.
+      if (!$rootScope.user) {
+        Formio.currentUser().then(function(user) {
+          $rootScope.user = user;
+        });
+      }
+
+      // Trigger when a logout occurs.
+      Formio.onLogout.then(function() {
+        $state.go('auth.login');
+      }, function() {
+        $state.go('auth.login');
+        FormioAlerts.addAlert({
+          type: 'danger',
+          message: 'Your session has expired. Please log in again.'
+        });
+      });
+
+      // Logout of form.io.
+      $rootScope.logout = function() {
+        Formio.logout();
+      };
+
+      $rootScope.isActive = function(state) {
+        return $state.current.name.indexOf(state) !== -1;
+      };
+
+      // Ensure they are logged.
+      $rootScope.$on('$stateChangeStart', function(event, toState) {
+        $rootScope.authenticated = !!Formio.getToken();
+        if (toState.name.substr(0, 4) === 'auth') { return; }
+        if(!$rootScope.authenticated) {
+          event.preventDefault();
+          $state.go('auth.login');
+        }
+      });
+
+      $rootScope.$on('$stateChangeSuccess', function() {
+        $rootScope.alerts = FormioAlerts.getAlerts();
+      });
     }
   ]);
