171e302ac11dba2a771aa56ce6ade459df87940b
Travis Tidwell <codertravist@gmail.com>
2015-06-11 23:31:04 -0500

    Fixing the signature component.

--- dist/formio.js
+++ dist/formio.js
@@ -2040,6 +2040,22 @@ app.directive('signature', function () {
     link: function (scope, element, attrs, ngModel) {
       if (!ngModel) { return; }
 
+      // Sets the dimension of a width or height.
+      var setDimension = function(dim) {
+        if (scope.component[dim].slice(-1) === '%') {
+          var percent = parseFloat(scope.component[dim].slice(0, -1)) / 100;
+          element[0][dim] = element.parent()[dim]() * percent;
+        }
+        else {
+          element[0][dim] = parseInt(scope.component[dim], 10);
+          scope.component[dim] += 'px';
+        }
+      };
+
+      // Set the width and height of the canvas.
+      setDimension('width');
+      setDimension('height');
+
       // Create the signature pad.
       /* global SignaturePad:false */
       var signaturePad = new SignaturePad(element[0], {
@@ -2058,9 +2074,7 @@ app.directive('signature', function () {
       element.css({
         'border-radius': '4px',
         'box-shadow': '0 0 5px rgba(0, 0, 0, 0.02) inset',
-        'border': '1px solid #f4f4f4',
-        'width': scope.component.width,
-        'height': scope.component.height
+        'border': '1px solid #f4f4f4'
       });
 
       function readSignature() {
@@ -2089,11 +2103,11 @@ app.run([
   ) {
     $templateCache.put('formio/components/signature.html', FormioUtils.fieldWrap(
       '<img ng-if="readOnly" ng-attr-src="{{data[component.key]}}" src="" />' +
-      '<span ng-if="!readOnly">' +
+      '<div ng-if="!readOnly" style="width: {{ component.width }}; height: {{ component.height }};">' +
         '<a class="btn btn-xs btn-default" style="position:absolute; left: 0; top: 0; z-index: 1000" ng-click="component.clearSignature()"><span class="glyphicon glyphicon-repeat"></span></a>' +
         '<canvas signature component="component" ng-model="data[component.key]"></canvas>' +
         '<div class="formio-signature-footer" style="text-align: center;color:#C3C3C3;">{{ component.footer }}</div>' +
-      '</span>'
+      '</div>'
     ));
   }
 ]);
