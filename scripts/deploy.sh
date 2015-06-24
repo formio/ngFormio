08ff9637e6c4832dbf072620e7ba1db1b2cfb580
Randall Knutson <randall@form.io>
2015-06-24 15:25:45 -0500

    FA-2 Add dns resolution for new environments.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+CURDIR=$(pwd)
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 # Get the tag name from a parameter to the script.
 TAG_NAME=$1
 # Get the environment from a parameter to the script.
@@ -7,9 +9,10 @@ ENVIRONMENT=$2
 EB_BUCKET=formio-ecs-bucket
 APPLICATION_NAME=form.io
 ENVIRONMENT_CONFIG=DevConfig
+HOSTED_ZONE_ID=ZITLBCTQPADKN
 MONGO_SERVER=ec2-52-25-221-252.us-west-2.compute.amazonaws.com
 MONGO1=mongodb://$MONGO_SERVER/$TAG_NAME
-DIR=aws-eb
+AWSDIR=aws-eb
 
 # Don't allow deploying to production. Sanity check.
 if [ $ENVIRONMENT == "prod" ]; then
@@ -26,15 +29,15 @@ fi
 
 # Create new application version
 echo "Creating $TAG_NAME.zip file."
-mkdir -p $DIR/versions/$TAG_NAME
-sed "s/<TAG>/$TAG_NAME/" < $DIR/Dockerrun.aws.json > $DIR/versions/$TAG_NAME/Dockerrun.aws.json
-mkdir -p $DIR/versions/$TAG_NAME/.ebextensions
-sed "s/<MONGO1>/$(echo $MONGO1 | sed -e 's/[\/&]/\\&/g')/" < $DIR/.ebextensions/app.config > $DIR/versions/$TAG_NAME/.ebextensions/app.config
-cd $DIR/versions/$TAG_NAME
+mkdir -p $AWSDIR/versions/$TAG_NAME
+sed "s/<TAG>/$TAG_NAME/" < $AWSDIR/Dockerrun.aws.json > $AWSDIR/versions/$TAG_NAME/Dockerrun.aws.json
+mkdir -p $AWSDIR/versions/$TAG_NAME/.ebextensions
+sed "s/<MONGO1>/$(echo $MONGO1 | sed -e 's/[\/&]/\\&/g')/" < $AWSDIR/.ebextensions/app.config > $AWSDIR/versions/$TAG_NAME/.ebextensions/app.config
+cd $AWSDIR/versions/$TAG_NAME
 zip -r ../$TAG_NAME.zip * .ebextensions/*
 cd ../../..
-echo "Uploading $DIR/versions/$TAG_NAME.zip to s3://$EB_BUCKET/$TAG_NAME.zip"
-aws s3 cp $DIR/versions/$TAG_NAME.zip s3://$EB_BUCKET/$TAG_NAME.zip
+echo "Uploading $AWSDIR/versions/$TAG_NAME.zip to s3://$EB_BUCKET/$TAG_NAME.zip"
+aws s3 cp $AWSDIR/versions/$TAG_NAME.zip s3://$EB_BUCKET/$TAG_NAME.zip
 
 echo "Creating application version $TAG_NAME."
 aws elasticbeanstalk create-application-version --application-name $APPLICATION_NAME --version-label $TAG_NAME --source-bundle S3Bucket=$EB_BUCKET,S3Key=$TAG_NAME.zip
@@ -45,8 +48,26 @@ EXISTS=`aws elasticbeanstalk describe-environments --environment-names $ENVIRONM
 # Either create or update the environment to the new version.
 if [[ -z $EXISTS ]]; then
   echo "No environment found. Creating new environment and setting application version to $TAG_NAME."
+  # TODO: Load the database here.
   aws elasticbeanstalk create-environment --application-name $APPLICATION_NAME --environment-name $ENVIRONMENT --version-label $TAG_NAME --template-name $ENVIRONMENT_CONFIG
-  # TODO: Probably need to load the database here.
+
+  # Wait for the load balancer to be initialized. We can't create the dns resource record until it is.
+  echo "Waiting for the load balancer to be initialized."
+  for a in {1..100}
+  do
+    LOAD_BALANCER=`aws elasticbeanstalk describe-environment-resources --environment-name $ENVIRONMENT --query 'EnvironmentResources.LoadBalancers[0].Name' --output text`
+    if [[ $LOAD_BALANCER != 'None' ]]; then
+      echo "$LOAD_BALANCER"
+      break
+    fi
+    echo -n "$a "
+    sleep 5
+  done
+  DNS_NAME=`aws elb describe-load-balancers --load-balancer-names $LOAD_BALANCER --query 'LoadBalancerDescriptions[0].CanonicalHostedZoneName' --output text`
+  ALIAS_ZONE=`aws elb describe-load-balancers --load-balancer-names $LOAD_BALANCER --query 'LoadBalancerDescriptions[0].CanonicalHostedZoneNameID' --output text`
+  sed -e "s/<ENVIRONMENT>/$ENVIRONMENT/" -e "s/<DNS_NAME>/$DNS_NAME/" -e "s/<ALIAS_ZONE>/$ALIAS_ZONE/" < $AWSDIR/DNSAdd.json > $AWSDIR/versions/$TAG_NAME/DNSAdd.json
+  echo "Adding recordset for $ENVIRONMENT.form.io."
+  aws route53 change-resource-record-sets --hosted-zone-id $HOSTED_ZONE_ID --change-batch file://$DIR/$AWSDIR/versions/$TAG_NAME/DNSAdd.json
 else
   echo "Existing environment found. Updating application version to $TAG_NAME."
   aws elasticbeanstalk update-environment --environment-name formio-env --version-label $TAG_NAME
