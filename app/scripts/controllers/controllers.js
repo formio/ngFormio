cfbd6361f8b154283397460277526b1963941f95
Rahat Ahmed <rahatarmanahmed@gmail.com>
2015-07-03 13:21:49 -0500

    FA-21 FA-22 FA-24 FA-25 Added Application Roles View/Create/Edit/Delete UI
    This obviously **won't work until FA-61 is merged**.
    
    Added initial layout for roles
    
    Added request logic for roles management
    
    Small bug fixes

--- app/scripts/controllers/controllers.js
+++ app/scripts/controllers/controllers.js
@@ -5,5 +5,6 @@ angular.module('formioApp.controllers', [
   'formioApp.controllers.team',
   'formioApp.controllers.user',
   'formioApp.controllers.import',
-  'formioApp.controllers.help'
+  'formioApp.controllers.help',
+  'formioApp.controllers.role'
 ]);
