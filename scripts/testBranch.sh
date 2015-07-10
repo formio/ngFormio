287cc9dba6c8f3b9626205905d37625cb4104076
Randall Knutson <randall@form.io>
2015-07-10 15:17:57 -0500

    Attempting to fix branch name.

--- scripts/testBranch.sh
+++ scripts/testBranch.sh
@@ -6,14 +6,12 @@ CIRCLE_SHA1=abc123
 echo "CIRCLE_SHA1:$CIRCLE_SHA1"
 SHORT_SHA=$(echo $CIRCLE_SHA1 | cut -b1-7)
 echo "SHORT_SHA:$SHORT_SHA"
-BRANCH=$(echo $CIRCLE_BRANCH | sed -E "s/\//-/g")
-echo "BRANCH:$BRANCH"
-RELEASE_REGEX=$(echo $BRANCH | sed -E "s/release-([a-zA-Z0-9\._]*).*/\1/" | sed -E "s/x/*/g")
+RELEASE_REGEX=$(echo $CIRCLE_BRANCH | sed -E "s/release\/([a-zA-Z0-9\._]*).*/\1/" | sed -E "s/x/*/g")
 echo "RELEASE_REGEX:$RELEASE_REGEX"
 LAST_RELEASE=$(git tag -l $RELEASE_REGEX | tail -n 1)
 echo "LAST_RELEASE:$LAST_RELEASE"
 SEMVER=$(if [ -z $LAST_RELEASE ]; then echo $RELEASE_REGEX | sed -E "s/\*/0/g"; else ./scripts/incrementVersion.sh -p $LAST_RELEASE; fi)
 echo "SEMVER:$SEMVER"
-TAG_NAME=$(if [[ $SEMVER =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then echo $SEMVER; else echo "$BRANCH-$SHORT_SHA"; fi)
+TAG_NAME=$(if [[ $SEMVER =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then echo $SEMVER; else echo "$CIRCLE_BRANCH-$SHORT_SHA" | sed -E "s/\//-/g"; fi)
 echo "TAG_NAME:$TAG_NAME"
 
