a0f0aaa00eee79c34080935f62a80eed68ec6a4a
Randall Knutson <randall@form.io>
2015-07-14 15:27:01 -0500

    Fix release a little.

--- scripts/getTagName.sh
+++ scripts/getTagName.sh
@@ -9,13 +9,15 @@ if [[ ! -z $TAG ]]; then
 fi
 
 # Create a tag.
-SHORT_SHA=$(echo $CIRCLE_SHA1 | cut -b1-7)
 RELEASE_REGEX=$(echo $CIRCLE_BRANCH | sed -E -e "s/release\/([a-zA-Z0-9\._]*).*/\1/" -e "s/x/*/g")
 LAST_RELEASE=$(git tag -l $RELEASE_REGEX | tail -n 1)
 SEMVER=$(if [ -z $LAST_RELEASE ]; then echo $RELEASE_REGEX | sed -E -e "s/\*/0/g"; else ./scripts/incrementVersion.sh -p $LAST_RELEASE; fi)
 TAG=$(if [[ $SEMVER =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then echo $SEMVER; else echo $CIRCLE_BRANCH-$SHORT_SHA | sed -E -e "s/\//-/g"; fi)
-if [[ $TAG =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+if [[ $SEMVER =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
   git tag $TAG
   git push --tags
+  echo $SEMVER
+else
+  SHORT_SHA=$(echo $CIRCLE_SHA1 | cut -b1-7)
+  echo $CIRCLE_BRANCH-$SHORT_SHA
 fi
-echo $TAG
