0c1fd19f95a4d6c126a24340c8f2aeb7be421d8e
Randall Knutson <randall@form.io>
2015-07-14 14:35:59 -0500

    a

--- scripts/getTagName.sh
+++ scripts/getTagName.sh
@@ -4,9 +4,9 @@
 BRANCH=$1
 SHA=$2
 SHORT_SHA=$(echo $SHA | cut -b1-7)
-RELEASE_REGEX=$(echo $BRANCH | sed -E -e "s/release\/([a-zA-Z0-9\._]*).*/\1/" -e "s/x/*/g")
+echo RELEASE_REGEX=$(echo $BRANCH | sed -E -e "s/release\/([a-zA-Z0-9\._]*).*/\1/" -e "s/x/*/g")
 LAST_RELEASE=$(git tag -l $RELEASE_REGEX | tail -n 1)
 SEMVER=$(if [ -z $LAST_RELEASE ]; then echo $RELEASE_REGEX | sed -E -e "s/\*/0/g"; else ./scripts/incrementVersion.sh -p $LAST_RELEASE; fi)
 TAG_NAME=$(if [[ $SEMVER =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then echo $SEMVER; else echo $BRANCH-$SHORT_SHA | sed -E -e "s/\//-/g"; fi)
-echo $TAG_NAME
+#echo $TAG_NAME
 
