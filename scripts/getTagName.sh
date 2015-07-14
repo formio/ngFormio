1376e9ca9a867f1568667f53065070623de556b6
Randall Knutson <randall@form.io>
2015-07-14 14:37:23 -0500

    a

--- scripts/getTagName.sh
+++ scripts/getTagName.sh
@@ -2,11 +2,12 @@
 # This file will return the correct tag based on the branch name and commit SHA.
 
 BRANCH=$1
+echo $BRANCH
 SHA=$2
 SHORT_SHA=$(echo $SHA | cut -b1-7)
-echo RELEASE_REGEX=$(echo $BRANCH | sed -E -e "s/release\/([a-zA-Z0-9\._]*).*/\1/" -e "s/x/*/g")
-LAST_RELEASE=$(git tag -l $RELEASE_REGEX | tail -n 1)
-SEMVER=$(if [ -z $LAST_RELEASE ]; then echo $RELEASE_REGEX | sed -E -e "s/\*/0/g"; else ./scripts/incrementVersion.sh -p $LAST_RELEASE; fi)
-TAG_NAME=$(if [[ $SEMVER =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then echo $SEMVER; else echo $BRANCH-$SHORT_SHA | sed -E -e "s/\//-/g"; fi)
+#echo RELEASE_REGEX=$(echo $BRANCH | sed -E -e "s/release\/([a-zA-Z0-9\._]*).*/\1/" -e "s/x/*/g")
+#LAST_RELEASE=$(git tag -l $RELEASE_REGEX | tail -n 1)
+#SEMVER=$(if [ -z $LAST_RELEASE ]; then echo $RELEASE_REGEX | sed -E -e "s/\*/0/g"; else ./scripts/incrementVersion.sh -p $LAST_RELEASE; fi)
+#TAG_NAME=$(if [[ $SEMVER =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then echo $SEMVER; else echo $BRANCH-$SHORT_SHA | sed -E -e "s/\//-/g"; fi)
 #echo $TAG_NAME
 
