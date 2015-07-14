7512ce7119a17cada7af92f3bad795f8b76d46a9
Randall Knutson <randall@form.io>
2015-07-14 09:24:29 -0500

    Testing out release process.

--- scripts/getTagName.sh
+++ scripts/getTagName.sh
@@ -2,11 +2,17 @@
 # This file will return the correct tag based on the branch name and commit SHA.
 
 BRANCH=$1
+echo $BRANCH
 SHA=$2
+echo SHA:$SHA
 SHORT_SHA=$(echo $SHA | cut -b1-7)
+echo SHORT_SHA:$SHORT_SHA
 RELEASE_REGEX=$(echo $BRANCH | sed -E -e "s/release\/([a-zA-Z0-9\._]*).*/\1/" -e "s/x/*/g")
+echo RELEASE_REGEX:$RELEASE_REGEX
 LAST_RELEASE=$(git tag -l $RELEASE_REGEX | tail -n 1)
+echo LAST_RELEASE:$LAST_RELEASE
 SEMVER=$(if [ -z $LAST_RELEASE ]; then echo $RELEASE_REGEX | sed -E -e "s/\*/0/g"; else ./scripts/incrementVersion.sh -p $LAST_RELEASE; fi)
-TAG_NAME=$(if [[ $SEMVER =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then echo $SEMVER; else echo $BRANCH-$SHORT_SHA | sed -E -e "s/\//-/g"; fi)
-echo $TAG_NAME
+echo SEMVER:$SEMVER
+export TAG_NAME=$(if [[ $SEMVER =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then echo $SEMVER; else echo $BRANCH-$SHORT_SHA | sed -E -e "s/\//-/g"; fi)
+echo TAG_NAME:$TAG_NAME
 
