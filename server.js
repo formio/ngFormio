0fdb3be9e8d4713e6606b9741059dd97eb26bf90
Zack Urben <zackurben@gmail.com>
2015-07-17 11:47:49 -0500

    FA-61 Fixing issue with formio /health check not progressing to api server /health check.

--- server.js
+++ server.js
@@ -70,16 +70,27 @@ app.get('/app/form/:formId/spec.html', function (req, res, next) {
 
 // Mount the api server.
 require('formio')(config.formio, function(formio) {
-  // The healthcheck.
-  app.get('/health', function(req, res) {
-    if (!formio.resources) { return res.status(500).send('No Resources'); }
-    if (!formio.resources.application.model) { return res.status(500).send('No application model'); }
+  // The formio app sanity endpoint.
+  app.get('/health', function(req, res, next) {
+    if (!formio.resources) {
+      return res.status(500).send('No Resources');
+    }
+    if (!formio.resources.application.model) {
+      return res.status(500).send('No application model');
+    }
+
     formio.resources.application.model.findOne({name: 'formio'}, function(err, result) {
-      if (err) { return res.status(500).send(err); }
-      if (!result) { return res.status(500).send('Formio application not found'); }
-      res.send('OK');
+      if (err) {
+        return res.status(500).send(err);
+      }
+      if (!result) {
+        return res.status(500).send('Formio application not found');
+      }
+
+      // Proceed with db schema sanity check middleware.
+      next();
     });
-  });
+  }, formio.updates.sanityCheck);
 
   app.use('/app/api', formio);
   console.log('Listening to port ' + config.port);
