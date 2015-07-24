05061500904471876db7d141999d1d6fcc5f7f65
Randall Knutson <randall@form.io>
2015-07-24 10:30:24 -0500

    Update codebase schema version to match database updates.

--- config.js
+++ config.js
@@ -23,7 +23,7 @@ module.exports = function() {
   config.formioHost = formioHost;
   config.debug = process.env.DEBUG || false;
   config.formio.domain = domain;
-  config.formio.schema = '0.0.0';
+  config.formio.schema = '1.0.0';
 
   if (process.env.MONGO1) {
     config.formio.mongo = [];
