b2e7b1fedaff653165ddb0249958963150e7b13c
Rahat Ahmed <rahatarmanahmed@gmail.com>
2015-08-07 15:39:01 -0500

    FA-92 Implemented button reset action

--- components/button.js
+++ components/button.js
@@ -16,6 +16,17 @@ app.config([
         action: 'submit',
         disableOnInvalid: true,
         theme: 'primary'
+      },
+      controller: function(settings, $scope) {
+        $scope.onClick = function() {
+          switch(settings.action) {
+            case 'submit':
+              return;
+            case 'reset':
+              $scope.resetForm();
+              break;
+          }
+        };
       }
     });
   }
@@ -26,13 +37,17 @@ app.run([
     $templateCache
   ) {
     $templateCache.put('formio/components/button.html',
-      '<button ng-class="{\'btn-block\': component.block}" class="btn btn-{{ component.theme }} btn-{{ component.size }}" ng-disabled="readOnly || form.submitting || (component.disableOnInvalid && form.$invalid)" ng-click="$emit(component.action)">' +
-        '<span ng-if="component.leftIcon" class="{{ component.leftIcon }}" aria-hidden="true"></span>' +
-        '<span ng-if="component.leftIcon && component.label">&nbsp;</span>' +
-        '{{ component.label }}' +
-        '<span ng-if="component.rightIcon && component.label">&nbsp;</span>' +
-        '<span ng-if="component.rightIcon" class="{{ component.rightIcon }}" aria-hidden="true"></span>' +
-        ' <i ng-if="component.action == \'submit\' && form.submitting" class="fa fa-spinner fa-pulse"></i>' +
+      '<button type="{{component.action == \'submit\' ? \'submit\' : \'button\'}}"' +
+        'ng-class="{\'btn-block\': component.block}"' +
+        'class="btn btn-{{ component.theme }} btn-{{ component.size }}"' +
+        'ng-disabled="readOnly || form.submitting || (component.disableOnInvalid && form.$invalid)"' +
+        'ng-click="onClick()">' +
+          '<span ng-if="component.leftIcon" class="{{ component.leftIcon }}" aria-hidden="true"></span>' +
+          '<span ng-if="component.leftIcon && component.label">&nbsp;</span>' +
+          '{{ component.label }}' +
+          '<span ng-if="component.rightIcon && component.label">&nbsp;</span>' +
+          '<span ng-if="component.rightIcon" class="{{ component.rightIcon }}" aria-hidden="true"></span>' +
+          ' <i ng-if="component.action == \'submit\' && form.submitting" class="fa fa-spinner fa-pulse"></i>' +
       '</button>'
     );
   }
