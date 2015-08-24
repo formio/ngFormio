ba0a6c1b94d07442318da2b410cb495f64d3aa66
Randall Knutson <randall@form.io>
2015-08-24 12:13:23 -0500

    Add app.js back in.

---
+++ app.js
@@ -0,0 +1,25 @@
+'use strict';
+
+require('dotenv').load({silent: true});
+var config = require('./config')();
+var express = require('express');
+var _ = require('lodash');
+var app = express();
+
+// Host the dynamic app configuration.
+//app.get('/config.js', function(req, res) {
+//  res.set('Content-Type', 'text/javascript');
+//  res.render('js/config.js', {
+//    forceSSL: config.https ? 'true' : 'false',
+//    domain: config.domain,
+//    appHost: config.host,
+//    apiHost: config.apiHost,
+//    formioHost: config.formioHost
+//  });
+//});
+
+// Add the formio Project.
+app.use('/', express.static(__dirname + '/dist'));
+
+console.log(' > Listening on port ' + config.port);
+app.listen(config.port);
