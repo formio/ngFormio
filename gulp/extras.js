7116bd4941a62198b9b3bbd0eb3b14482451d40d
Randall Knutson <randall@form.io>
2015-08-26 16:56:47 -0500

    Couple cleanups.

--- gulp/extras.js
+++ gulp/extras.js
@@ -4,6 +4,9 @@ module.exports = function(gulp, plugins) {
       'src/brochure/**/*'
     ]).pipe(gulp.dest('dist/'));
     gulp.src([
+      'bower_components/**/*'
+    ]).pipe(gulp.dest('dist/bower_components'));
+    gulp.src([
       'src/app/*.*',
       '!src/app/*.html'
     ], {
