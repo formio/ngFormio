b57c36f0ef95d47bced6ca81dec7dbcec5e8e193
Randall Knutson <randall@form.io>
2015-09-14 15:28:10 -0500

    FA-338 Add dummy controller so feedback works on edit page.

--- dist/formio.js
+++ dist/formio.js
@@ -1011,6 +1011,9 @@ app.directive('formioElement', [
       scope: false,
       link: function(scope, element) {
         element.replaceWith($compile($templateCache.get(scope.template))(scope));
+      },
+      controller: function() {
+        // This is required for some reason as it will occasionally throw an error without it.
       }
     };
   }
--- formio.js
+++ formio.js
@@ -1009,6 +1009,9 @@ app.directive('formioElement', [
       scope: false,
       link: function(scope, element) {
         element.replaceWith($compile($templateCache.get(scope.template))(scope));
+      },
+      controller: function() {
+        // This is required for some reason as it will occasionally throw an error without it.
       }
     };
   }
