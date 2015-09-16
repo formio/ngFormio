5b2fb01c68785a3d5cb713b6fcc52b6126c16c22
Randall Knutson <randall@form.io>
2015-09-16 11:16:04 -0500

    Rename Api Explorer to API.

--- src/scripts/controllers/form.js
+++ src/scripts/controllers/form.js
@@ -73,11 +73,11 @@ app.config([
         templateUrl: 'views/form/permission/index.html',
         controller: 'FormSubmissionsController'
       })
-      .state('project.form.explorer', {
-        url: '/explorer',
+      .state('project.form.api', {
+        url: '/api',
         parent: 'project.form',
-        templateUrl: 'views/form/explorer/index.html',
-        controller: 'ExplorerController'
+        templateUrl: 'views/form/api/index.html',
+        controller: 'ApiController'
       });
 
     var formStates = {};
@@ -762,7 +762,7 @@ app.constant('SubmissionAccessLabels', {
 });
 
 
-app.controller('ExplorerController', [
+app.controller('ApiController', [
   '$scope',
   '$state',
   'Formio',
