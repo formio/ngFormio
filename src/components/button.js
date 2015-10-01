84957a7cece8dcf21fcef50830e733a9c8684223
Rahat Ahmed <rahatarmanahmed@gmail.com>
2015-10-01 10:06:52 -0500

    Added oauth action

--- src/components/button.js
+++ src/components/button.js
@@ -20,15 +20,74 @@ module.exports = function (app) {
           theme: 'primary'
         },
         controller: function (settings, $scope) {
-          $scope.onClick = function () {
-            switch (settings.action) {
+          $scope.onClick = function() {
+            switch(settings.action) {
               case 'submit':
                 return;
               case 'reset':
                 $scope.resetForm();
                 break;
+              case 'oauth':
+                if($scope.form && !settings.oauth) {
+                  alert('You must assign this button to an OAuth action before it will work.');
+                  break;
+                }
+                $scope.openOAuth(settings.oauth);
+                break;
             }
           };
+
+          $scope.openOAuth = function(settings) {
+            var params = {
+              response_type: 'code',
+              client_id: settings.clientID,
+              redirect_uri: window.location.origin || window.location.protocol + '//' + window.location.host,
+              state: settings.state
+            };
+            params = Object.keys(params).map(function(key) {
+              return key + '=' + encodeURIComponent(params[key]);
+            }).join('&');
+
+            var url = settings.authURI + '?' + params;
+
+            // TODO: make window options from oauth settings, have better defaults
+            var popup = window.open(url, settings.provider, 'width=1020,height=618');
+            var interval = setInterval(function() {
+              try {
+                var popupHost = popup.location.host;
+                var currentHost = window.location.host;
+                if(popupHost === currentHost && popup.location.search) {
+                  popup.close();
+                  var params = popup.location.search.substr(1).split('&').reduce(function(params, param) {
+                    var split = param.split('=');
+                    params[split[0]] = split[1];
+                    return params;
+                  }, {});
+                  if(settings.state !== params.state) {
+                    throw new Error('OAuth state does not match!' + settings.state + ' ' + params.state);
+                  }
+                  var submission = { data: {}, oauth: {} };
+                  submission.oauth[settings.provider] = params;
+                  $scope.form.submitting = true;
+                  $scope.formio.saveSubmission(submission)
+                  .then(function(submission) {
+                    // Trigger the form submission.
+                    $scope.$emit('formSubmission', submission);
+                  })
+                  .finally(function() {
+                    $scope.form.submitting = false;
+                  });
+                }
+              }
+              catch(error) {
+                console.error(error);
+              }
+              if(!popup || popup.closed || popup.closed === undefined) {
+                clearInterval(interval);
+              }
+            }, 100);
+          };
+
         }
       });
     }
