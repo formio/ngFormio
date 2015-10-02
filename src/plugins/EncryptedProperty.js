3d1b02aaa2cdc157a119505333df24eb3f2d2982
Zack Urben <zackurben@gmail.com>
2015-10-01 21:22:33 -0500

    Adding fa-434.

--- src/plugins/EncryptedProperty.js
+++ src/plugins/EncryptedProperty.js
@@ -6,14 +6,16 @@
  *  https://github.com/joegoldbeck/mongoose-encryption/issues/12
  */
 var crypto = require('crypto');
+var debug = require('debug')('formio:plugins:EncryptedProperty');
 
 /**
- * Encrypted property plugin
+ * Mongoose encrypted property plugin. Encrypt a single field on the given schema.
  *
- * @param   {Schema}  schema
- * @param   {Object}  options
- * @param   {String}  options.secret
- * @param   {String}  options.plainName
+ * @param {Schema} schema
+ *   The mongoose schema to use.
+ * @param {Object} options
+ *   options.secret - The key used for crypto.
+ *   options.plainName - The plainName used for the encrypted field.
  */
 module.exports = function(schema, options) {
   // Validate options
@@ -26,14 +28,18 @@ module.exports = function(schema, options) {
   }
 
   /**
-   * Encrypt some text
-   * @param   {String} secret
-   * @param   {Object} mixed
+   * Encrypt the given data with the given secret.
+   *
+   * @param {String} secret
+   *   The key used for crypto.
+   * @param {Object}
+   *   The content to encrypt.
+   *
    * @returns {Buffer}
    */
   function encrypt(secret, mixed) {
-    if (mixed === undefined) {
-      return undefined;
+    if (!secret || !mixed) {
+      return null;
     }
 
     var cipher = crypto.createCipher('aes-256-cbc', secret);
@@ -43,34 +49,38 @@ module.exports = function(schema, options) {
       cipher.update(decryptedJSON),
       cipher.final()
     ]);
-  };
+  }
 
   /**
    * Decrypt some text
    *
-   * @param   {String} secret
-   * @param   {Buffer} cipherbuffer
+   * @param {String} secret
+   *   The key used for crypto.
+   * @param {Buffer} cipherbuffer
+   *   The content to decrypt.
+   *
    * @returns {Object}
+   *   Decrypted content.
    */
   function decrypt(secret, cipherbuffer) {
-    if (cipherbuffer === undefined) {
-      return undefined;
+    if (!secret || !cipherbuffer) {
+      return null;
     }
 
     var decipher = crypto.createDecipher('aes-256-cbc', secret);
-    var decryptedJSON = '';
-    try {
-      decryptedJSON = Buffer.concat([
-        decipher.update(cipherbuffer), // Buffer contains encrypted utf8
-        decipher.final()
-      ]);
-    }
-    catch (e) {
-      decryptedJSON = '{}';
-    }
+    var decryptedJSON = Buffer.concat([
+      decipher.update(cipherbuffer), // Buffer contains encrypted utf8
+      decipher.final()
+    ]);
 
-    return JSON.parse(decryptedJSON);  // This can throw a exception
-  };
+    var data = {};
+    try {
+      data = JSON.parse(decryptedJSON);
+    }
+    catch(e) {}
+
+    return data;
+  }
 
   // Add a Buffer property to store the encrypted data
   var encryptedName = options.plainName + '_encrypted';
@@ -79,24 +89,52 @@ module.exports = function(schema, options) {
   schema.add(bufferProperty);
 
   // Add a Virtual property to access with decrypted data
-  schema.virtual(options.plainName).get(function() {
-    // Decrypt the value
-    if (this[encryptedName]) {
-      return decrypt(options.secret, this[encryptedName]);
-    }
-    return undefined;
-  })
-  .set(function(value) {
-    // Encrypt and set the value
-    this[encryptedName] = encrypt(options.secret, value);
-  });
+  schema.virtual(options.plainName)
+    .get(function() {
+      // Decrypt the value
+      if (this[encryptedName]) {
+        var plaintext = decrypt(options.secret, this[encryptedName]);
+        debug('plaintext: ' + JSON.stringify(plaintext));
+        return plaintext;
+      }
 
-  // decrypt data when converted toJSON
-  // FIXME.  This may not work with more than one encrypted field
+      return null;
+    })
+    .set(function(value) {
+      // Encrypt and set the value
+      var ciphertext = encrypt(options.secret, value);
+      debug('Encrypting: ' + JSON.stringify(value));
+      this[encryptedName] = ciphertext;
+    });
+
+  // Decrypt data when converted using toJSON.
   schema.set('toJSON', {
     transform: function(doc, ret, opts) {
       delete ret[encryptedName];
-      ret[options.plainName] = decrypt(options.secret, doc[encryptedName]);
+      var temp = decrypt(options.secret, doc[encryptedName]);
+
+      if (temp) {
+        ret[options.plainName] = temp;
+        debug('JSON Transform: ' + JSON.stringify(ret[options.plainName]));
+      }
+
+      debug(ret);
+      return ret;
+    }
+  });
+
+  // Decrypt data when converted using toObject.
+  schema.set('toObject', {
+    transform: function(doc, ret, opts) {
+      delete ret[encryptedName];
+      var temp = decrypt(options.secret, doc[encryptedName]);
+
+      if (temp) {
+        ret[options.plainName] = temp;
+        debug('Object Transform: ' + JSON.stringify(ret[options.plainName]));
+      }
+
+      debug(ret);
       return ret;
     }
   });
