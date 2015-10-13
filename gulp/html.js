b7c8a01d576132e16c915bc341aa02224701ad2e
Rahat Ahmed <rahatarmanahmed@gmail.com>
2015-10-13 18:03:12 -0500

    Fixed wiredep and style tasks interferring with each other + tasks not returning streams and finishing early

--- gulp/html.js
+++ gulp/html.js
@@ -1,16 +1,21 @@
 module.exports = function(gulp, plugins) {
   return function () {
+    var stream = require('merge-stream')();
+
     var assets = plugins.useref.assets({searchPath: ['.tmp', 'src', '.']});
-    gulp.src('src/*.html')
+    stream.add(gulp.src('src/*.html')
       .pipe(assets)
       .pipe(plugins.if('*.js', plugins.uglify()))
       .pipe(plugins.if('*.css', plugins.csso()))
       .pipe(assets.restore())
       .pipe(plugins.useref())
       .pipe(plugins.if('*.html', plugins.minifyHtml({conditionals: true, loose: true})))
-      .pipe(gulp.dest('dist'));
-    gulp.src('src/config.js')
+      .pipe(gulp.dest('dist')));
+
+    stream.add(gulp.src('src/config.js')
       .pipe(plugins.replace('var serverHost = host;', 'var serverHost = \'localhost:3000\';'))
-      .pipe(gulp.dest('.tmp'));
+      .pipe(gulp.dest('.tmp')));
+
+    return stream;
   };
 };
