99cdf2bcc16804e2e38ce6fa7fe48c608478ec87
Randall Knutson <randall@form.io>
2015-10-13 21:27:11 -0500

    Revert "Added watchify in watch task"
    This reverts commit 9bafc0759e894bfe3fa5134f944970a5a7d975a8.

--- gulpfile.js
+++ gulpfile.js
@@ -4,15 +4,12 @@ var gulp = require('gulp');
 var plugins = require('gulp-load-plugins')();
 plugins.source = require('vinyl-source-stream');
 plugins.browserify = require('browserify');
-plugins.watchify = require('watchify');
 
 gulp.task('clean', require('del').bind(null, ['dist']));
+gulp.task('watch', require('./gulp/watch')(gulp, plugins));
 gulp.task('jshint', require('./gulp/jshint')(gulp, plugins));
-gulp.task('scripts:formio', require('./gulp/scripts')(gulp, plugins));
-gulp.task('scripts:formio-full', require('./gulp/scripts-full')(gulp, plugins));
-gulp.task('scripts', ['scripts:formio', 'scripts:formio-full']);
+gulp.task('scripts', require('./gulp/scripts')(gulp, plugins));
 gulp.task('build', ['clean', 'scripts'], function() {
   gulp.start('jshint');
 });
-gulp.task('watch', require('./gulp/scripts')(gulp, plugins, true));
 gulp.task('default', ['build', 'watch']);
