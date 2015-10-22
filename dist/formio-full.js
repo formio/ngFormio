821812bce222ed1ed72a905443d0a3144901fde0
Randall Knutson <randall@form.io>
2015-10-22 15:04:25 -0500

    FA-472 Make resources query based on a field.

--- dist/formio-full.js
+++ dist/formio-full.js
@@ -60543,49 +60543,26 @@ module.exports = function (app) {
           }
           if (settings.resource) {
             var formio = new Formio($scope.formio.projectUrl + '/form/' + settings.resource);
-            var params = {};
-
-            // If they wish to filter the results.
-            if (settings.selectFields) {
-              params.select = settings.selectFields;
-            }
-
-            if (settings.searchExpression && settings.searchFields) {
-              var search = new RegExp(settings.searchExpression);
               $scope.refreshSubmissions = function (input) {
-                if (!input) {
-                  return [];
-                }
-                var matches = input.match(search);
-                var shouldRequest = false;
-                if (matches && matches.length > 1) {
-                  angular.forEach(settings.searchFields, function (field, index) {
-                    if ((matches.length > (index + 1)) && matches[index + 1]) {
-                      params[field] = matches[index + 1];
-                      shouldRequest = true;
-                    }
-                  });
-                }
+              var params = {};
+              // If they wish to return only some fields.
+              if (settings.selectFields) {
+                params.select = settings.selectFields;
+              }
+              if (settings.searchFields && input) {
+                angular.forEach(settings.searchFields, function (field, index) {
+                  params[field] = input;
+                });
+              }
+              // Load the submissions.
+              formio.loadSubmissions({
+                params: params
+              }).then(function (submissions) {
+                $scope.selectItems = submissions || [];
+              });
+            };
 
-                // Do not request unless we have parameters.
-                if (!shouldRequest) {
-                  return;
-                }
-              };
-            }
-            else {
-
-              // Load all submissions.
-              $scope.refreshSubmissions = function () {
-              };
-            }
-
-            // Load the submissions.
-            formio.loadSubmissions({
-              params: params
-            }).then(function (submissions) {
-              $scope.selectItems = submissions || [];
-            });
+            $scope.refreshSubmissions();
           }
         },
         settings: {
@@ -60623,7 +60600,7 @@ module.exports = function (app) {
         '<ui-select-match placeholder="{{ component.placeholder }}">' +
         '<formio-select-item template="component.template" item="$item || $select.selected" select="$select"></formio-select-item>' +
         '</ui-select-match>' +
-        '<ui-select-choices repeat="item in selectItems | filter: $select.search" refresh="refreshSubmissions($select.search)" refresh-delay="1000">' +
+        '<ui-select-choices repeat="item in selectItems | filter: $select.search" refresh="refreshSubmissions($select.search)" refresh-delay="250">' +
         '<formio-select-item template="component.template" item="item" select="$select"></formio-select-item>' +
         '</ui-select-choices>' +
         '</ui-select>' +
