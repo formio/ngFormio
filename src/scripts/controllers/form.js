3c73d33a59990f1c78192bc9e60be862c2d34289
Randall Knutson <randall@form.io>
2015-11-19 14:52:50 -0600

    Fix jshint issues.

--- src/scripts/controllers/form.js
+++ src/scripts/controllers/form.js
@@ -592,24 +592,24 @@ app.controller('FormActionEditController', [
           };
           switch(value) {
             case 'field':
-              fields['_field'] = '';
+              fields._field = '';
               break;
             case 'value':
             case 'increment':
             case 'decrement':
-              fields['_value'] = '';
+              fields._value = '';
               break;
           }
-          angular.element('#form-group-' + key + '_value').css('display', fields['_value']);
-          angular.element('#form-group-' + key + '_field').css('display', fields['_field']);
-        }
+          angular.element('#form-group-' + key + '_value').css('display', fields._value);
+          angular.element('#form-group-' + key + '_field').css('display', fields._field);
+        };
 
         if(!$scope.currentProject.settings || !$scope.currentProject.settings.hubspot || !$scope.currentProject.settings.hubspot.apikey) {
           FormioAlerts.warn('<i class="glyphicon glyphicon-exclamation-sign"></i> You have not yet configured your Hubspot API key. You can configure your Hubspot API key in your <a href="#/project/'+$scope.projectId+'/settings/hubspot">Project Settings</a>.');
           $scope.formDisabled = true;
         }
         FormioUtils.eachComponent(actionInfo.settingsForm.components, function(component) {
-          var result = component.key.match(/settings\[(.*)_action\]/)
+          var result = component.key.match(/settings\[(.*)_action\]/);
           if (result) {
             $timeout(function() {
               showFields(result[1], $scope.action.data.settings[result[1] + '_action']);
