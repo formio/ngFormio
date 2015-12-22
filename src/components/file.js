47c43167dcb33479f72023916b046ba451870016
Travis Tidwell <travis@form.io>
2015-12-22 09:51:46 -0600

    Adding directives to the file uploading.

--- src/components/file.js
+++ src/components/file.js
@@ -19,22 +19,80 @@ module.exports = function (app) {
       });
     }
   ]);
-  app.controller('formioFile', [
+
+  app.directive('formioFileList', [function() {
+    return {
+      restrict: 'E',
+      replace: true,
+      scope: {
+        files: '=',
+        form: '=',
+        readOnly: '='
+      },
+      templateUrl: 'formio/components/formio-file-list.html',
+      controller: [
+        '$scope',
+        function ($scope) {
+          $scope.removeFile = function (event, index) {
+            event.preventDefault();
+            $scope.files.splice(index, 1);
+          };
+
+          $scope.fileSize = function (a, b, c, d, e) {
+            return (b = Math, c = b.log, d = 1024, e = c(a) / c(d) | 0, a / b.pow(d, e)).toFixed(2) + ' ' + (e ? 'kMGTPEZY'[--e] + 'B' : 'Bytes');
+          };
+        }
+      ]
+    };
+  }]);
+
+  app.directive('formioFile', [function() {
+    return {
+      restrict: 'E',
+      replace: true,
+      scope: {
+        file: '=',
+        form: '='
+      },
+      template: '<a href="{{ file.url }}" ng-click="getFile($event)" target="_blank">{{ file.name }}</a>',
+      controller: [
+        '$scope',
+        'Formio',
+        '$window',
+        function (
+          $scope,
+          Formio,
+          $window
+        ) {
+          $scope.getFile = function (evt) {
+            // If this is not a public file, get a signed url and open in new tab.
+            if ($scope.file.acl !== 'public-read') {
+              evt.preventDefault();
+              Formio.request($scope.form + '/storage/s3?bucket=' + $scope.file.bucket + '&key=' + $scope.file.key, 'GET')
+                .then(function (response) {
+                  $window.open(response.url, '_blank');
+                })
+                .catch(function (response) {
+                  // Is alert the best way to do this? User is expecting an immediate notification due to attempting to download a file.
+                  alert(response);
+                });
+            }
+          };
+        }
+      ]
+    };
+  }]);
+
+  app.controller('formioFileUpload', [
     '$scope',
-    '$window',
     'Upload',
     'Formio',
     function(
       $scope,
-      $window,
       Upload,
       Formio
     ) {
       $scope.fileUploads = {};
-
-      $scope.removeFile = function(index) {
-        $scope.data[$scope.component.key].splice(index, 1);
-      };
 
       $scope.removeUpload = function(index) {
         console.log(index);
@@ -48,25 +106,6 @@ module.exports = function (app) {
       if ($scope.data && $scope.data[$scope.component.key][0] === '') {
         $scope.data[$scope.component.key].splice(0, 1);
       }
-
-      $scope.fileSize = function(a,b,c,d,e){
-        return (b=Math,c=b.log,d=1024,e=c(a)/c(d)|0,a/b.pow(d,e)).toFixed(2)+' '+(e?'kMGTPEZY'[--e]+'B':'Bytes');
-      };
-
-      $scope.getFile = function(evt, file) {
-        // If this is not a public file, get a signed url and open in new tab.
-        if (file.acl !== 'public-read') {
-          evt.preventDefault();
-          Formio.request($scope.formio.formUrl + '/storage/s3?bucket=' + file.bucket + '&key=' + file.key, 'GET')
-            .then(function(response) {
-              $window.open(response.url, '_blank');
-            })
-            .catch(function(response) {
-              // Is alert the best way to do this? User is expecting an immediate notification due to attempting to download a file.
-              alert(response);
-            });
-        }
-      };
 
       $scope.upload = function(files) {
         if ($scope.component.storage && files && files.length) {
@@ -128,55 +167,58 @@ module.exports = function (app) {
   ]);
   app.run([
     '$templateCache',
-    'FormioUtils',
-    function ($templateCache,
-              FormioUtils) {
-      $templateCache.put('formio/components/file.html',
-        '<label ng-if="component.label && !component.hideLabel" for="{{ component.key }}" class="control-label" ng-class="{\'field-required\': component.validate.required}">{{ component.label }}</label>' +
-        '<span ng-if="!component.label && component.validate.required" class="glyphicon glyphicon-asterisk form-control-feedback field-required-inline" aria-hidden="true"></span>' +
-        '<div ng-controller="formioFile">' +
-        '<table class="table table-striped table-bordered" ng-if="data[component.key].length">' +
+    function (
+      $templateCache
+    ) {
+
+      $templateCache.put('formio/components/formio-file-list.html',
+        '<table class="table table-striped table-bordered">' +
           '<thead>' +
             '<tr>' +
-              '<td style="width:1%;white-space:nowrap;"></td>' +
+              '<td ng-if="!readOnly" style="width:1%;white-space:nowrap;"></td>' +
               '<th>File Name</th>' +
               '<th>Size</th>' +
             '</tr>' +
           '</thead>' +
           '<tbody>' +
-            '<tr ng-repeat="file in data[component.key] track by $index">' +
-              '<td style="width:1%;white-space:nowrap;"><a  ng-if="!readOnly" href="#" ng-click="removeFile($index)" style="padding: 2px 4px;" class="btn btn-sm btn-default"><span class="glyphicon glyphicon-remove"></span></a></td>' +
-              '<td><a href="{{ file.url }}" ng-click="getFile($event, file)" target="_blank">{{ file.name }}</a></td>' +
+            '<tr ng-repeat="file in files track by $index">' +
+              '<td ng-if="!readOnly" style="width:1%;white-space:nowrap;"><a ng-if="!readOnly" href="#" ng-click="removeFile($event, $index)" style="padding: 2px 4px;" class="btn btn-sm btn-default"><span class="glyphicon glyphicon-remove"></span></a></td>' +
+              '<td><formio-file file="file" form="form"></formio-file></td>' +
               '<td>{{ fileSize(file.size) }}</td>' +
             '</tr>' +
           '</tbody>' +
-        '</table>' +
-        '<div ng-if="!readOnly">' +
-        ' <div ngf-drop="upload($files)" class="fileSelector" ngf-drag-over-class="' + "'" + 'fileDragOver' + "'" + '" ngf-multiple="component.multiple"><span class="glyphicon glyphicon-cloud-upload"></span>Drop files to attach, or <a href="#" ngf-select="upload($files)" ngf-multiple="component.multiple">browse</a>.</div>' +
-        ' <div ng-if="!component.storage" class="alert alert-warning">No storage has been set for this field. File uploads are disabled until storage is set up.</div>' +
-        ' <div ngf-no-file-drop>File Drag/Drop is not supported for this browser</div>' +
-        '</div>' +
-        '<div ng-repeat="fileUpload in fileUploads track by $index" ng-class="{' + "'has-error'" + ': fileUpload.status === '+ "'error'" +'}" class="file">' +
-        ' <div class="row">' +
-        '   <div class="fileName control-label col-sm-10">{{ fileUpload.name }} <span ng-click="removeUpload(fileUpload.name)" class="glyphicon glyphicon-remove"></span></div>' +
-        '   <div class="fileSize control-label col-sm-2 text-right">{{ fileSize(fileUpload.size) }}</div>' +
-        ' </div>' +
-        ' <div class="row">' +
-        '   <div class="col-sm-12">' +
-        '   <span ng-if="fileUpload.status === ' + "'progress'" + '">' +
-        '     <div class="progress">' +
-        '       <div class="progress-bar" role="progressbar" aria-valuenow="{{fileUpload.progress}}" aria-valuemin="0" aria-valuemax="100" style="width:{{fileUpload.progress}}%">' +
-        '         <span class="sr-only">{{fileUpload.progress}}% Complete</span>' +
-        '       </div>' +
-        '     </div>' +
-        '   </span>' +
-        '   <div ng-if="!fileUpload.status !== ' + "'progress'" + '" class="bg-{{ fileUpload.status }} control-label">' +
-        '     {{ fileUpload.message }} ' +
-        '   </div>' +
-        '   </div>' +
-        ' </div>' +
-        '</div>' +
-      '</div>'
+        '</table>'
+      );
+
+      $templateCache.put('formio/components/file.html',
+        '<label ng-if="component.label && !component.hideLabel" for="{{ component.key }}" class="control-label" ng-class="{\'field-required\': component.validate.required}">{{ component.label }}</label>' +
+        '<span ng-if="!component.label && component.validate.required" class="glyphicon glyphicon-asterisk form-control-feedback field-required-inline" aria-hidden="true"></span>' +
+        '<div ng-controller="formioFileUpload">' +
+          '<formio-file-list files="data[component.key]" form="formio.formUrl"></formio-file-list>' +
+          '<div ng-if="!readOnly">' +
+            '<div ngf-drop="upload($files)" class="fileSelector" ngf-drag-over-class="' + "'" + 'fileDragOver' + "'" + '" ngf-multiple="component.multiple"><span class="glyphicon glyphicon-cloud-upload"></span>Drop files to attach, or <a href="#" ngf-select="upload($files)" ngf-multiple="component.multiple">browse</a>.</div>' +
+            '<div ng-if="!component.storage" class="alert alert-warning">No storage has been set for this field. File uploads are disabled until storage is set up.</div>' +
+            '<div ngf-no-file-drop>File Drag/Drop is not supported for this browser</div>' +
+          '</div>' +
+          '<div ng-repeat="fileUpload in fileUploads track by $index" ng-class="{' + "'has-error'" + ': fileUpload.status === '+ "'error'" +'}" class="file">' +
+            '<div class="row">' +
+              '<div class="fileName control-label col-sm-10">{{ fileUpload.name }} <span ng-click="removeUpload(fileUpload.name)" class="glyphicon glyphicon-remove"></span></div>' +
+              '<div class="fileSize control-label col-sm-2 text-right">{{ fileSize(fileUpload.size) }}</div>' +
+            '</div>' +
+            '<div class="row">' +
+              '<div class="col-sm-12">' +
+                '<span ng-if="fileUpload.status === ' + "'progress'" + '">' +
+                  '<div class="progress">' +
+                    '<div class="progress-bar" role="progressbar" aria-valuenow="{{fileUpload.progress}}" aria-valuemin="0" aria-valuemax="100" style="width:{{fileUpload.progress}}%">' +
+                      '<span class="sr-only">{{fileUpload.progress}}% Complete</span>' +
+                    '</div>' +
+                  '</div>' +
+                '</span>' +
+                '<div ng-if="!fileUpload.status !== ' + "'progress'" + '" class="bg-{{ fileUpload.status }} control-label">{{ fileUpload.message }}</div>' +
+              '</div>' +
+            '</div>' +
+          '</div>' +
+        '</div>'
       );
     }
   ]);
