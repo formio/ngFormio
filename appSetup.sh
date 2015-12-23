32d3dda6c9e06e613ed4d59bec2bce1ea5a15ff3
Rahat Ahmed <rahatarmanahmed@gmail.com>
2015-12-23 11:00:04 -0600

    Removed git submodules

--- appSetup.sh
+++ appSetup.sh
@@ -2,11 +2,4 @@
 
 npm install
 bower install --allow-root -s -F
-rm -rf bower_components/ngForm*
-git submodule update --init --recursive
-cd bower_components/ngFormBuilder
-bower install
-cd ../ngFormio
-bower install
-cd ../..
 gulp
