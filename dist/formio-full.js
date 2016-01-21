65fbe9a64f17c9b47d611cb52dfd9ee7d91c94d6
Randall Knutson <randall@form.io>
2016-01-21 12:03:44 -0600

    Fix issue with actions settings form due to formio-utils.

--- dist/formio-full.js
+++ dist/formio-full.js
@@ -47533,10 +47533,7 @@ module.exports = {
     if (!components) return;
 
     components.forEach(function(component) {
-      if (component.tree) {
-        fn(component);
-      }
-      else if (component.columns && Array.isArray(component.columns)) {
+      if (component.columns && Array.isArray(component.columns)) {
         component.columns.forEach(function(column) {
           eachComponent(column.components, fn);
         });
@@ -47553,6 +47550,10 @@ module.exports = {
       }
 
       else {
+        fn(component);
+      }
+      // If the component is a tree, be sure to add it back in as well.
+      if (component.tree) {
         fn(component);
       }
     });
--- dist/formio.js
+++ dist/formio.js
@@ -102,10 +102,7 @@ module.exports = {
     if (!components) return;
 
     components.forEach(function(component) {
-      if (component.tree) {
-        fn(component);
-      }
-      else if (component.columns && Array.isArray(component.columns)) {
+      if (component.columns && Array.isArray(component.columns)) {
         component.columns.forEach(function(column) {
           eachComponent(column.components, fn);
         });
@@ -122,6 +119,10 @@ module.exports = {
       }
 
       else {
+        fn(component);
+      }
+      // If the component is a tree, be sure to add it back in as well.
+      if (component.tree) {
         fn(component);
       }
     });
