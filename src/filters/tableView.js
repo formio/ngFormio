4dfd3b0afe3c63d450045f63969bbbe094173467
Travis Tidwell <travis@form.io>
2016-02-23 11:12:24 -0600

    Fixed an issue with the tableView filter.

--- src/filters/tableView.js
+++ src/filters/tableView.js
@@ -8,12 +8,10 @@ module.exports = [
     $interpolate
   ) {
     return function(value, component) {
-      value = Formio.fieldData(value, component);
-      if (!value) {
-        return '';
-      }
       var componentInfo = formioComponents.components[component.type];
-      if (!componentInfo.tableView) return value;
+      if (!componentInfo.tableView) {
+        return value;
+      }
       if (component.multiple && (value.length > 0)) {
         var values = [];
         angular.forEach(value, function(arrayValue) {
