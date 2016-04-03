571aa01b944f705f8b8831803db3d01a2b9887e0
Travis Tidwell <travis@form.io>
2016-04-02 23:46:57 -0500

    Fixed the debug flag on builds.

--- gulp/scripts-complete.js
+++ gulp/scripts-complete.js
@@ -1,7 +1,8 @@
 module.exports = function(gulp, plugins) {
   return function() {
     return plugins.browserify({
-      entries: './src/formio-complete.js'
+      entries: './src/formio-complete.js',
+      debug: false
     })
       .bundle()
       .pipe(plugins.source('formio-complete.js'))
