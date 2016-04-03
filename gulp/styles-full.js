4ab2c3b0d320ea64fbd633893cd5c3f9290ecb9e
Travis Tidwell <travis@form.io>
2016-04-02 23:44:36 -0500

    Fixed issue with styles removing.

--- gulp/styles-full.js
+++ gulp/styles-full.js
@@ -3,7 +3,7 @@ module.exports = function(gulp, plugins) {
     return gulp.src(plugins.bowerFiles({
         includeSelf: true
     }))
-      .pipe(plugins.filter('*.css'))
+      .pipe(plugins.filter('**/*.css'))
       .pipe(plugins.concat('formio-full.css'))
       .pipe(gulp.dest('dist'))
       .pipe(plugins.cssnano())
