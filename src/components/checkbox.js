50632618d995772ddcfede9c2e78a8097b3b2a4e
Zack Urben <zackurben@gmail.com>
2016-05-18 11:56:33 -0500

    Fixing type issues with checkboxes, leading to strange behavior with the default behavior of true.

--- src/components/checkbox.js
+++ src/components/checkbox.js
@@ -6,7 +6,19 @@ module.exports = function(app) {
       formioComponentsProvider.register('checkbox', {
         title: 'Check Box',
         template: 'formio/components/checkbox.html',
-        controller: [function() {}], // This empty controller is required to fix a bug with checkboxes on ui view change - fa-825.
+        controller: ['$scope', function($scope) {
+          // FA-850 - Ensure the checked value is always a boolen object when loaded, then unbind the watch.
+          var loadComplete = $scope.$watch('data.' + $scope.component.key, function() {
+            var boolean = {
+              true: true,
+              false: false
+            };
+            if ($scope.data && $scope.data[$scope.component.key] && !($scope.data[$scope.component.key] instanceof Boolean)) {
+              $scope.data[$scope.component.key] = boolean[$scope.data[$scope.component.key]] || false;
+              loadComplete();
+            }
+          });
+        }],
         settings: {
           input: true,
           inputType: 'checkbox',
