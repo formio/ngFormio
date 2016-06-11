b7bf02852191e2d257afd3d6d1deccec3272d350
Travis Tidwell <travis@form.io>
2016-06-10 22:01:35 -0500

    Update project files.

--- src/config.js
+++ src/config.js
@@ -36,14 +36,14 @@ angular.module('formioApp').constant('AppConfig', {
       "title": "Default",
       "name": "default",
       "description": "A default project with User and Admin resources and their respective authentication forms.",
-      "template": "https://cdn.rawgit.com/formio/formio-app-basic/2.0.1/src/project.json",
+      "template": "https://cdn.rawgit.com/formio/formio-app-basic/2.1.1/src/project.json",
       "thumbnail": "https://app.form.io/formio-app-basic/assets/images/default-thumb.png"
     },
     {
       "title": "Sales Quote",
       "name": "salesquoteapp",
       "description": "A sales quoting project that lets agents login and manage their opportunities, customers, and contracts.",
-      "template": "https://cdn.rawgit.com/formio/formio-app-salesquote/2.1.0/src/project.json",
+      "template": "https://cdn.rawgit.com/formio/formio-app-salesquote/2.1.1/src/project.json",
       "thumbnail": "https://app.form.io/formio-app-salesquote/src/assets/images/salesquote-thumb.png"
     },
     {
@@ -57,21 +57,21 @@ angular.module('formioApp').constant('AppConfig', {
       "title": "Prize Drawing",
       "name": "prizedrawingapp",
       "description": "A raffle entry project that lets participants register for a chance to win a prize! Also includes admin interface for prize drawing selection.",
-      "template": "https://cdn.rawgit.com/formio/formio-app-prizedrawing/2.x/src/project.json",
+      "template": "https://cdn.rawgit.com/formio/formio-app-prizedrawing/2.1.0/src/project.json",
       "thumbnail": "https://app.form.io/formio-app-prizedrawing/assets/images/prizedrawing-thumb.png"
     },
     {
       "title": "Form Manager",
       "name": "form-manager",
       "description": "A form management application that includes Form Building capabilities as well as Data management.",
-      "template": "https://cdn.rawgit.com/formio/formio-app-formio/2.0.1/src/project.json",
+      "template": "https://cdn.rawgit.com/formio/formio-app-formio/2.1.0/src/project.json",
       "thumbnail": "https://app.form.io/formio-app-formio/src/assets/images/form-manager-thumb.png"
     },
     {
       "title": "ToDo List",
       "name": "todoapp",
       "description": "An example to-do project that lets users register, login, and keep track of their to-do list.",
-      "template": "https://cdn.rawgit.com/formio/formio-app-todo/2.0.1/src/project.json",
+      "template": "https://cdn.rawgit.com/formio/formio-app-todo/2.1.0/src/project.json",
       "thumbnail": "https://app.form.io/formio-app-todo/src/assets/images/todo-thumbnail.png"
     }
   ]
