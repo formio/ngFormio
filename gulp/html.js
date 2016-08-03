3bdaaebe2460a61c39958989acc54285ab8a6fb9
Travis Tidwell <travis@form.io>
2016-08-03 00:22:01 -0500

    Adding back the html build routine.

--- gulp/html.js
+++ gulp/html.js
@@ -3,9 +3,12 @@ module.exports = function(gulp, plugins) {
     var stream = require('merge-stream')();
 
     stream.add(gulp.src('src/*.html')
-      .pipe(plugins.debug({title: 'html:'}))
       .pipe(plugins.useref({searchPath: ['.tmp', 'src', '.']}))
-      .pipe(plugins.minifyHtml({conditionals: true, loose: true}))
+      .pipe(plugins.debug({title: 'html:'}))
+      .pipe(plugins.if('**/app.js', plugins.uglify()))
+      .pipe(plugins.if('**/plugins.js', plugins.uglify()))
+      .pipe(plugins.if('*.css', plugins.csso()))
+      .pipe(plugins.if('*.html', plugins.minifyHtml({conditionals: true, loose: true})))
       .pipe(gulp.dest('dist')));
 
     stream.add(gulp.src('src/config.js')
