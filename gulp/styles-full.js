adbefe9311862b3656d9c49be2dafe5476f40e97
Travis Tidwell <travis@form.io>
2016-08-16 15:59:57 -0500

    Fixed some styling issues.

--- gulp/styles-full.js
+++ gulp/styles-full.js
@@ -6,7 +6,7 @@ module.exports = function(gulp, plugins) {
       .pipe(plugins.filter('**/*.css'))
       .pipe(plugins.concat('formio-full.css'))
       .pipe(gulp.dest('dist'))
-      .pipe(plugins.cssnano())
+      .pipe(plugins.cssnano({zindex: false}))
       .pipe(plugins.rename('formio-full.min.css'))
       .pipe(gulp.dest('dist'));
   };
