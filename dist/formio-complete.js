574b3c60625c0535376bb0b8d59a20789f2d36e7
Travis Tidwell <travis@form.io>
2016-11-03 16:17:34 -0500

    Version bump and upgrade formio.js

--- dist/formio-complete.js
+++ dist/formio-complete.js
@@ -1,4 +1,4 @@
-/*! ng-formio v2.3.4 | https://unpkg.com/ng-formio@2.3.4/LICENSE.txt */
+/*! ng-formio v2.3.5 | https://unpkg.com/ng-formio@2.3.5/LICENSE.txt */
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.formio = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(_dereq_,module,exports){
 (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
@@ -47079,8 +47079,9 @@ var dropbox = function(formio) {
         }
 
         xhr.open('POST', formio.formUrl + '/storage/dropbox');
+        var token = false;
         try {
-          var token = localStorage.getItem('formioToken');
+          token = localStorage.getItem('formioToken');
         }
         catch (e) {
           // Swallow error.
@@ -47092,6 +47093,13 @@ var dropbox = function(formio) {
       });
     },
     downloadFile: function(file) {
+      var token = false;
+      try {
+        token = localStorage.getItem('formioToken');
+      }
+      catch (e) {
+        // Swallow error.
+      }
       file.url = formio.formUrl + '/storage/dropbox?path_lower=' + file.path_lower + (token ? '&x-jwt-token=' + token : '');
       return Promise.resolve(file);
     }
@@ -47196,8 +47204,9 @@ var s3 = function(formio) {
 
         pre.setRequestHeader('Accept', 'application/json');
         pre.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+        var token = false;
         try {
-          var token = localStorage.getItem('formioToken');
+          token = localStorage.getItem('formioToken');
         }
         catch (e) {
           // swallow error.
