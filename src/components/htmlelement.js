4b95e8be2733d715f55b64e2d1a6edfd6d77eaf8
Zack Urben <zackurben@gmail.com>
2016-12-05 16:19:19 -0600

    FOR-69 - Fix issue with htmlelement component where invalid html wouldnt render, thus making it impossible to remove from the form builder.

--- src/components/htmlelement.js
+++ src/components/htmlelement.js
@@ -12,14 +12,23 @@ module.exports = function(app) {
         },
         templateUrl: 'formio/components/htmlelement-directive.html',
         link: function($scope) {
-          var createElement = function() {
-            var element = angular.element(
-              '<' + $scope.component.tag + '>' + '</' + $scope.component.tag + '>'
-            );
+          var displayError = function(msg) {
+            $scope.parseError = 'Invalid HTML: ' + msg.toString();
+          };
 
+          $scope.$watch('component', function createElement() {
+            if (!$scope.component.tag) {
+              return displayError('No tag given');
+            }
+
+            var element = angular.element('<' + $scope.component.tag + '>' + '</' + $scope.component.tag + '>');
             element.html($filter('formioTranslate')($scope.component.content));
 
-            element.attr('class', $scope.component.className);
+            // Add the css classes if supplied.
+            if ($scope.component.className) {
+              element.attr('class', $scope.component.className);
+            }
+
             angular.forEach($scope.component.attrs, function(attr) {
               if (!attr.attr) return;
               element.attr(attr.attr, attr.value);
@@ -28,18 +37,19 @@ module.exports = function(app) {
             try {
               $scope.html = $sanitize(element.prop('outerHTML'));
               $scope.parseError = null;
+
+              // If the sanitized html is empty, it was invalid; Create a visible error so we still render something.
+              if (!$scope.html) {
+                return displayError(element.prop('outerHTML'));
+              }
             }
             catch (err) {
               // Isolate the message and store it.
               $scope.parseError = err.message
-              .split('\n')[0]
-              .replace('[$sanitize:badparse]', '');
+                .split('\n')[0]
+                .replace('[$sanitize:badparse]', '');
             }
-          };
-
-          createElement();
-
-          $scope.$watch('component', createElement, true);
+          }, true);
         }
       };
   }]);
