66603835e2c49d9a2a5ab2eccb0592c1e44dd7bd
Zack Urben <zackurben@gmail.com>
2016-12-23 14:47:48 -0600

    FOR-276 - Fixing issue with old custom validators which used quoted string replace.

--- src/directives/customValidator.js
+++ src/directives/customValidator.js
@@ -10,6 +10,37 @@ module.exports = function() {
       ) {
         return;
       }
+
+      var _get = function(item, path, def) {
+        if (!item) {
+          return def || undefined;
+        }
+        if (!path) {
+          return item;
+        }
+
+        // If the path is a string, turn it into an array.
+        if (typeof path === 'string') {
+          path = path.split('.')
+        }
+        // If the path is an array, take the first element, and recurse its path
+        if (path instanceof Array) {
+          var current = path.shift();
+          if (item.hasOwnProperty(current)) {
+            // If there are no more path items, stop here.
+            if (path.length === 0) {
+              return item[current];
+            }
+
+            return _get(item[current], path);
+          }
+
+          return undefined;
+        }
+
+        return undefined;
+      };
+
       ctrl.$validators.custom = function(modelValue, viewValue) {
         var valid = true;
         /*eslint-disable no-unused-vars */
@@ -22,7 +53,7 @@ module.exports = function() {
 
         var custom = scope.component.validate.custom;
         custom = custom.replace(/({{\s{0,}(.*[^\s]){1}\s{0,}}})/g, function(match, $1, $2) {
-          return 'scope.submission.data.' + $2;
+          return _get(scope.submission.data, $2);
         });
 
         try {
