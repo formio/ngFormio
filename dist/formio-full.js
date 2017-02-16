86ff4db64c4c38183b39b8ba23c12120b87916dd
Travis Tidwell <travis@form.io>
2017-02-16 14:03:11 -0600

    Turned the month default to empty string.

--- dist/formio-full.js
+++ dist/formio-full.js
@@ -73819,24 +73819,24 @@ module.exports = function(app) {
       controller: ['$scope', function($scope) {
         if ($scope.builder) return;
         $scope.months = [
-          {value: 0, label: $scope.component.fields.month.placeholder},
-          {value: 1, label: 'January'},
-          {value: 2, label: 'February'},
-          {value: 3, label: 'March'},
-          {value: 4, label: 'April'},
-          {value: 5, label: 'May'},
-          {value: 6, label: 'June'},
-          {value: 7, label: 'July'},
-          {value: 8, label: 'August'},
-          {value: 9, label: 'September'},
-          {value: 10, label: 'October'},
-          {value: 11, label: 'November'},
-          {value: 12, label: 'December'}
+          {value: '', label: $scope.component.fields.month.placeholder},
+          {value: '01', label: 'January'},
+          {value: '02', label: 'February'},
+          {value: '03', label: 'March'},
+          {value: '04', label: 'April'},
+          {value: '05', label: 'May'},
+          {value: '06', label: 'June'},
+          {value: '07', label: 'July'},
+          {value: '08', label: 'August'},
+          {value: '09', label: 'September'},
+          {value: '10', label: 'October'},
+          {value: '11', label: 'November'},
+          {value: '12', label: 'December'}
         ];
 
         $scope.date = {
           day: '',
-          month: 0,
+          month: '',
           year: ''
         };
       }],
@@ -73851,7 +73851,7 @@ module.exports = function(app) {
               : ngModel.$viewValue;
             if ((parts instanceof Array) && parts.length === 3) {
               scope.date.day = parts[(scope.component.dayFirst ? 0 : 1)];
-              scope.date.month = parseInt(parts[(scope.component.dayFirst ? 1 : 0)], 10);
+              scope.date.month = parts[(scope.component.dayFirst ? 1 : 0)];
               scope.date.year = parts[2];
             }
           }
