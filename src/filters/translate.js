f43b4bcd18da42611294177a7600fc684348b96e
Abdalla Mohamed Elabd <abdalla.mohamed.elabd@gmail.com>
2017-03-01 13:18:21 +0100

    allow translating strings with angular-gettext

--- src/filters/translate.js
+++ src/filters/translate.js
@@ -1,22 +1,42 @@
 module.exports = [
   '$filter',
+  '$injector',
   function(
-    $filter
+    $filter,
+    $injector
   ) {
     return function(text, key, builder) {
+      /**
+       * Lookup the available translate libraries, currently supports:
+       * angular-translate: @see https://github.com/angular-translate/angular-translate
+       * angular-gettext: @see https://github.com/rubenv/angular-gettext
+       */
+      var $translate, gettextCatalog;
+      if ($injector.has('$translate')) {
+        $translate = $injector.get('$translate');
+      }
+      else if ($injector.has('gettextCatalog')) {
+        gettextCatalog = $injector.get('gettextCatalog');
+      }
       if (builder) return text;
       try {
-        var translate = $filter('translate');
+        // Translate text using either angular-translate or angular-gettext
+        var translateText = function(text) {
+          if ($translate) return $translate(text);
+          if (gettextCatalog) return gettextCatalog.getString(text);
+          return text;
+        };
+
         // Allow translating by field key which helps with large blocks of html.
         if (key) {
-          var result = translate(key);
+          var result = translateText(key);
           if (result === key) {
-            result = translate(text);
+            result = translateText(text);
           }
           return result;
         }
 
-        return translate(text);
+        return translateText(text);
       }
       catch (e) {
         return text;
