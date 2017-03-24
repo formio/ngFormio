f0aaf1726c56415a4c7109411e67e5ee9088b63f
Travis Tidwell <travis@form.io>
2017-03-23 20:15:18 -0500

    Upgrade renderer with latest version.

--- dist/formio-complete.js
+++ dist/formio-complete.js
@@ -1,4 +1,4 @@
-/*! ng-formio v2.14.0 | https://unpkg.com/ng-formio@2.14.0/LICENSE.txt */
+/*! ng-formio v2.14.1 | https://unpkg.com/ng-formio@2.14.1/LICENSE.txt */
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.formio = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(_dereq_,module,exports){
 (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
@@ -47968,7 +47968,10 @@ var Formio = function () {
       }
     };
 
-    this.projectUrl = this.projectUrl || hostName;
+    if (!this.projectUrl || this.projectUrl === this.base) {
+      this.projectUrl = hostName;
+    }
+
     if (!this.noProject) {
       // Determine the projectUrl and projectId
       if (path.search(/(^|\/)(project)($|\/)/) !== -1) {
