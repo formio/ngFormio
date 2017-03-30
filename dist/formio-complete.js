fd18fbd7c5149805b8e372795584ed0fcd9c118a
Randall Knutson <randall@form.io>
2017-03-29 17:15:54 -0700

    Revert "FOR-383: Fixed an issue where the select dropdown was not populating content when using URL settings."
    This reverts commit 13c49812daf533d5f924237b4b340e097c0abf18.

--- dist/formio-complete.js
+++ dist/formio-complete.js
@@ -67094,7 +67094,20 @@ module.exports = function(app) {
               if (settings.dataSrc === 'values') {
                 return item.value;
               }
-              return valueProp ? _get(item, valueProp) : item;
+
+              // Allow dot notation in the value property.
+              if (valueProp.indexOf('.') !== -1) {
+                var parts = valueProp.split('.');
+                var prop = item;
+                for (var i in parts) {
+                  if (prop.hasOwnProperty(parts[i])) {
+                    prop = prop[parts[i]];
+                  }
+                }
+                return prop;
+              }
+
+              return valueProp ? item[valueProp] : item;
             };
 
             $scope.refreshItems = function() {
@@ -67104,80 +67117,48 @@ module.exports = function(app) {
               $scope.refreshItems(input, url);
             });
 
-            var refreshing = false;
-            var refreshValue = function() {
-              if (refreshing) {
-                return;
-              }
-              refreshing = true;
-              var tempData = $scope.data[settings.key];
-              $scope.data[settings.key] = settings.multiple ? [] : '';
-              if (!settings.clearOnRefresh) {
-                $timeout(function() {
-                  $scope.data[settings.key] = tempData;
-                  refreshing = false;
-                  $scope.$emit('selectLoaded', $scope.component);
-                });
-              }
-              else {
-                refreshing = false;
-                $scope.$emit('selectLoaded', $scope.component);
-              }
-            };
-
-            // Refresh the items when ready.
-            var refreshItemsWhenReady = function() {
-              initialized.promise.then(function() {
-                var refreshPromise = $scope.refreshItems();
-                if (refreshPromise) {
-                  refreshPromise.then(refreshValue);
-                }
-                else {
-                  refreshValue();
-                }
-              });
-            };
-
             // Add a watch if they wish to refresh on selection of another field.
             if (settings.refreshOn) {
+              var refreshing = false;
+              var refreshValue = function() {
+                if (refreshing) {
+                  return;
+                }
+                refreshing = true;
+                var tempData = $scope.data[settings.key];
+                $scope.data[settings.key] = settings.multiple ? [] : '';
+                if (!settings.clearOnRefresh) {
+                  $timeout(function() {
+                    $scope.data[settings.key] = tempData;
+                    refreshing = false;
+                    $scope.$emit('selectLoaded', $scope.component);
+                  });
+                }
+                else {
+                  refreshing = false;
+                  $scope.$emit('selectLoaded', $scope.component);
+                }
+              };
+
+              // Refresh the value.
+              var refreshValueWhenReady = function() {
+                initialized.promise.then(function() {
+                  var refreshPromise = $scope.refreshItems();
+                  if (refreshPromise) {
+                    refreshPromise.then(refreshValue);
+                  }
+                  else {
+                    refreshValue();
+                  }
+                });
+              };
               if (settings.refreshOn === 'data') {
-                $scope.$watch('data', refreshItemsWhenReady, true);
+                $scope.$watch('data', refreshValueWhenReady, true);
                 return;
               }
 
-              $scope.$watch('data.' + settings.refreshOn, refreshItemsWhenReady);
-              $scope.$watch('submission.data.' + settings.refreshOn, refreshItemsWhenReady);
-            }
-            else {
-              // Watch for the data to be set, and ensure the value is set properly.
-              var dataWatch = $scope.$watch('data.' + settings.key, function(value) {
-                if (value) {
-                  initialized.promise.then(function() {
-                    dataWatch();
-                    // Iterate through the list of items and see if our value exists...
-                    var found = false;
-                    for (var i=0; i < $scope.selectItems.length; i++) {
-                      var item = $scope.selectItems[i];
-                      var selectItem = $scope.getSelectItem(item);
-                      if (_isEqual(selectItem, value)) {
-                        found = true;
-                        break;
-                      }
-                    }
-
-                    // If the item is not found in the select items array, then add it manually.
-                    if (!found) {
-                      var itemValue = value;
-                      if (valueProp) {
-                        itemValue = {};
-                        _set(itemValue, valueProp, value);
-                      }
-                      $scope.selectItems.push(itemValue);
-                    }
-                    refreshValue();
-                  });
-                }
-              });
+              $scope.$watch('data.' + settings.refreshOn, refreshValueWhenReady);
+              $scope.$watch('submission.data.' + settings.refreshOn, refreshValueWhenReady);
             }
 
             switch (settings.dataSrc) {
@@ -67264,8 +67245,6 @@ module.exports = function(app) {
                       }
                     });
                   }
-                  options.params.skip = parseInt(options.params.skip, 10);
-                  options.params.limit = parseInt(options.params.limit, 10);
                   selectItems = selectItems.slice(options.params.skip, options.params.skip + options.params.limit);
                   setResult(selectItems, append);
                   return initialized.resolve($scope.selectItems);
