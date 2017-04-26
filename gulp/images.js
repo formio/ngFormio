803c5512fdd5900493630f045301072e8904bb3e
Travis Tidwell <travis@form.io>
2017-04-26 17:01:36 -0500

    Updated build.

--- gulp/images.js
+++ gulp/images.js
@@ -2,14 +2,19 @@ module.exports = function(gulp, plugins) {
   return function () {
     var stream = require('merge-stream')();
 
-    stream.add(gulp.src('src/images/**/*')
+    stream.add(gulp.src('./src/images/**/*', {base: './src/images/'})
       .pipe(plugins.debug({title: 'images:'}))
       .pipe(plugins.cache(plugins.imagemin({
         progressive: true,
         interlaced: true,
         svgoPlugins: [{cleanupIDs: false}]
       })))
-      .pipe(gulp.dest('dist/images'))
+      .pipe(gulp.dest('./dist/images/'))
+    );
+
+    stream.add(gulp.src('./src/images/**/*.svg', {base: './src/images/'})
+      .pipe(plugins.debug({title: 'images:'}))
+      .pipe(gulp.dest('./dist/images/'))
     );
 
     // Copy over images from kendo-ui
