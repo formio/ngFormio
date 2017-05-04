67c97b602f1e3be230a0f84014191cb9bec497ad
Zack Urben <zackurben@gmail.com>
2017-05-04 16:46:51 -0500

    Adding initial tableview to columns layout components.

--- src/components/columns.js
+++ src/components/columns.js
@@ -9,10 +9,54 @@ module.exports = function(app) {
         group: 'layout',
         settings: {
           input: false,
+          tableView: true,
           key: 'columns',
           columns: [{components: []}, {components: []}]
         },
-        viewTemplate: 'formio/componentsView/columns.html'
+        viewTemplate: 'formio/componentsView/columns.html',
+        tableView: function(data, component, $interpolate, componentInfo) {
+          var view = '<table class="table table-striped table-bordered"><thead><tr>';
+
+          angular.forEach(component.columns, function() {
+            view += '<th>Column (' + component.key + ')</th>';
+          });
+          view += '</tr></thead>';
+          view += '<tbody>';
+          view += '<tr>';
+
+          angular.forEach(component.columns, function(column) {
+            angular.forEach(column.components, function(component) {
+              // If the component has a defined tableView, use that, otherwise try and use the raw data as a string.
+              var info = componentInfo.components.hasOwnProperty(component.type)
+                ? componentInfo.components[component.type]
+                : {};
+              if (info.tableView) {
+                view += '<td>' +
+                  info.tableView(
+                    data && component.key && (data.hasOwnProperty(component.key) ? data[component.key] : ''),
+                    component,
+                    $interpolate,
+                    componentInfo
+                  ) + '</td>';
+              }
+              else {
+                view += '<td>';
+                if (component.prefix) {
+                  view += component.prefix;
+                }
+                view += data && component.key && (data.hasOwnProperty(component.key) ? data[component.key] : '');
+                if (component.suffix) {
+                  view += ' ' + component.suffix;
+                }
+                view += '</td>';
+              }
+            });
+          });
+
+          view += '</tr>';
+          view += '</tbody></table>';
+          return view;
+        }
       });
     }
   ]);
