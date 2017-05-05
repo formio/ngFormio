8b00fbfd29937ee7ad2290ea18ff1f831bc2998d
Zack Urben <zackurben@gmail.com>
2017-05-05 15:18:11 -0500

    Adding well layout components to the kendo grid display.

--- src/scripts/controllers/form.js
+++ src/scripts/controllers/form.js
@@ -1664,7 +1664,7 @@ app.controller('FormSubmissionsController', [
             return;
           }
 
-          if (['container', 'datagrid'].indexOf(component.type) !== -1) {
+          if (['container', 'datagrid', 'well'].indexOf(component.type) !== -1) {
             FormioUtils.eachComponent(component.components, function(component) {
               if (component.key) {
                 componentHistory.push(component.key);
@@ -1673,7 +1673,7 @@ app.controller('FormSubmissionsController', [
           }
 
           columns.push(getKendoCell(component));
-        });
+        }, true);
 
         columns.push(
           {
