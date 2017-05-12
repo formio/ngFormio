f953ddceb594bc6660a36fbd1c26d0cc5d52a454
Zack Urben <zackurben@gmail.com>
2017-05-12 17:01:40 -0500

    Fixing bug where editing a team would not take you to the edit page, but project overview page.

--- src/scripts/controllers/project.js
+++ src/scripts/controllers/project.js
@@ -289,7 +289,7 @@ app.controller('ProjectController', [
       });
 
       // Calculate the users highest role within the project.
-      $q.all([userTeamsPromise, projectTeamsPromise]).then(function() {
+      $scope.highestRoleLoaded = $q.all([userTeamsPromise, projectTeamsPromise]).then(function() {
         var roles = _.has($scope.user, 'roles') ? $scope.user.roles : [];
         var teams = _($scope.userTeams ? $scope.userTeams : [])
           .map('_id')
@@ -1791,24 +1791,27 @@ app.controller('ProjectSettingsController', [
     AppConfig,
     $interval
   ) {
-    if (!$scope.highestRole || ($scope.highestRole && ['team_read', 'team_write'].indexOf($scope.highestRole) !== -1)) {
-      $state.go('project.overview');
-      return;
-    }
-
-    $scope.currentSection.title = 'Settings';
-    $scope.currentSection.icon = 'fa fa-cogs';
-    $scope.currentSection.help = 'https://help.form.io/userguide/#settings-project';
-    // Go to first settings section
-    if($state.current.name === 'project.settings') {
-      $state.go('project.settings.project', {location: 'replace'});
-    }
-
     $scope.loadProjectPromise.then(function() {
       // Mask child scope's reference to currentProject with a clone
       // Parent reference gets updated when we reload after saving
       $scope.currentProject.plan = $scope.currentProject.plan || 'basic';
       $scope.currentProject = _.cloneDeep($scope.currentProject);
+
+      $scope.highestRoleLoaded.then(function() {
+        // Check the highest role, after the project has been loaded.
+        if (!$scope.highestRole || ($scope.highestRole && ['team_read', 'team_write'].indexOf($scope.highestRole) !== -1)) {
+          $state.go('project.overview');
+          return;
+        }
+
+        $scope.currentSection.title = 'Settings';
+        $scope.currentSection.icon = 'fa fa-cogs';
+        $scope.currentSection.help = 'https://help.form.io/userguide/#settings-project';
+        // Go to first settings section
+        if($state.current.name === 'project.settings') {
+          $state.go('project.settings.project', {location: 'replace'});
+        }
+      });
     });
 
     $scope.addKey = function() {
