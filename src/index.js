ccb486580f2772d5c5a538a3ee005c407173287c
Randall Knutson <randall@form.io>
2017-07-14 12:51:59 -0500

    Clean up old code.

--- src/index.js
+++ src/index.js
@@ -3,18 +3,5 @@ import util from 'formiojs/utils';
 
 import './providers';
 import './directives';
+
 const app = angular.module('formio', []);
-
-/**
- * Create the formio provider.
- */
-app.provider('Formio', FormioProvider);
-
-/**
- * Provides a way to register the Formio scope.
- */
-app.factory('FormioUtils', util);
-
-app.directive('formio', Formio);
-
-//app.directive('formioWizard', FormioWizard);
