b23eed6ca33485b1720cccc56a651eb4a89ada0f
Randall Knutson <randall@form.io>
2017-08-31 13:53:02 -0500

    Try adding a wait.

--- test/features/environments.spec.js
+++ test/features/environments.spec.js
@@ -102,6 +102,7 @@ module.exports = function (actions) {
 
     describe('Tag the release', function() {
       actions.clickOnLink('Settings');
+      actions.waitForActionToComplete(200);
       actions.clickOnLink('Deployment');
       actions.clickOnLink('Create Version Tag');
       actions.enterTextInField('#tag', '0.0.2');
