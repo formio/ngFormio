056026c68143008f24ac3ed361a61420c6b68d30
Randall Knutson <randall@form.io>
2017-09-08 13:06:34 -0500

    Remove environment switcher logic from config.

--- src/config.js
+++ src/config.js
@@ -1,49 +1,19 @@
-// If environment configurations are passed in the querystring, first override existing configurations.
-var query = Qs.parse(window.location.search.substr(1));
-if (query.hasOwnProperty('environments') && query.hasOwnProperty('currentEnvironment')) {
-  localStorage.setItem('environments', JSON.stringify(query.environments));
-  localStorage.setItem('currentEnvironment', JSON.stringify(query.currentEnvironment));
-  // Ensure they are logged out since we are switching environments
-  localStorage.removeItem('formioToken');
-  localStorage.removeItem('formioUser');
-  // Rebuild the window url and replace without the environment querystrings.
-  if (history.replaceState) {
-    var url = window.location.protocol
-      + "//"
-      + window.location.host
-      + window.location.pathname
-      + window.location.hash;
-
-    history.replaceState({page: url}, document.getElementsByTagName('title')[0].innerHTML, url);
-  }
-}
-
 var host = window.location.host;
-var environment = JSON.parse(localStorage.getItem('currentEnvironment'));
 var protocol = window.location.protocol;
 var serverHost, apiProtocol;
 var pathType = 'Subdomains';
 
-
-if (environment) {
-  var parts = environment.url.split('//');
-  apiProtocol = parts[0];
-  serverHost = parts[1];
-  pathType = environment.type || 'Subdomains';
+serverHost = host;
+apiProtocol = (serverHost.split(':')[0] !== 'localhost') ? 'https:' : protocol;
+var parts = serverHost.split('.');
+if (parts[0] === 'portal') {
+  parts.shift();
+  serverHost = parts.join('.');
 }
-else {
-  serverHost = host;
-  apiProtocol = (serverHost.split(':')[0] !== 'localhost') ? 'https:' : protocol;
-  var parts = serverHost.split('.');
-  if (parts[0] === 'portal' || parts[0] === 'beta') {
-    parts.shift();
-    serverHost = parts.join('.');
-  }
-  //if (parts[0] === 'beta') {
-  //  parts.shift();
-  //  parts[0] = 'test-' + parts[0];
-  //  serverHost = parts.join('.');
-  //}
+if (parts[0] === 'beta') {
+  parts.shift();
+  parts[0] = 'test-' + parts[0];
+  serverHost = parts.join('.');
 }
 
 // Force portal and server to match protocols if not on localhost.
@@ -54,41 +24,15 @@ if (apiProtocol !== protocol && ['localhost', 'portal.localhost', 'lvh.me', 'por
       + "//"
       + window.location.host
       + window.location.pathname;
+      + window.location.hash;
 
-    // Add environment information if set.
-    if (localStorage.getItem('environments') && localStorage.getItem('currentEnvironment')) {
-      url += '?' + Qs.stringify({
-          environments: JSON.parse(localStorage.getItem('environments')),
-          currentEnvironment: JSON.parse(localStorage.getItem('currentEnvironment'))
-        });
-    }
-    url += window.location.hash;
-
-    // If running under http, confirm switching back to https to prevent looping.
-    if (protocol === 'http:') {
-      if (confirm('The API server is running under HTTPS. Do you want to switch the portal as well? (Recommended)')) {
-        setTimeout(function() {
-          window.location.replace(url);
-        }, 100);
-      }
-    }
-    else {
-      setTimeout(function() {
-        window.location.replace(url);
-      }, 100);
-    }
+    window.location.replace(url);
   }
 }
 
 var appBase = protocol + '//' + host;
 var apiBase = apiProtocol + '//api.' + serverHost;
-var formioBase = apiProtocol + '//formio.' + serverHost;
-if (['form.io', 'test-form.io'].indexOf(serverHost) ===  -1 && pathType !== 'Subdomains') {
-  apiBase = apiProtocol + '//' + serverHost;
-  formioBase = apiProtocol + '//' + serverHost + '/formio';
-}
-
-angular.module('formioApp').constant('AppConfig', {
+var formioBase = apiProtocol + '//formio.' + serverHost;angular.module('formioApp').constant('AppConfig', {
   appVersion: 'APP_VERSION',
   copyrightYear: (new Date()).getFullYear().toString(),
   pathType: pathType,
