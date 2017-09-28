b099f375622deb2b387b3155b380969b6065d327
Yury Rybak <Yury.Rybak@Softensity.com>
2017-09-28 19:06:45 +0300

    FOR-677 - Added secret to project settings

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -299,6 +299,11 @@ angular
           parent: 'project.env',
           templateUrl: 'views/project/env/settings/cors/index.html'
         })
+        .state('project.env.settings.security', {
+          url: '/settings/security',
+          parent: 'project.env',
+          templateUrl: 'views/project/env/settings/security/index.html'
+        })
         .state('project.env.database', {
           url: '/database',
           abstract: true,
