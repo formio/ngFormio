9fe02c17cd8462cfa8d67d189c9cb32ae02510e1
rakesh <rxb162130@utdallas.edu>
2017-10-19 17:45:28 -0500

    Tests added

--- test/test.js
+++ test/test.js
@@ -46,10 +46,10 @@ describe("Formio Tests", function () {
   // require('./features/environments.spec')(actions);
   // //require('./features/createResource2.spec')(actions);
   // //require('./features/createForm.spec')(actions);
-  // require('./features/settingsBasic.spec')(actions);
-  // require('./features/settingsIndependent.spec')(actions);
-  // require('./features/settingsTeamPro.spec')(actions);
-  // require('./features/settingsEnterprise.spec')(actions );
+  require('./features/settingsBasic.spec')(actions);
+  require('./features/settingsIndependent.spec')(actions);
+  require('./features/settingsTeamPro.spec')(actions);
+  require('./features/settingsEnterprise.spec')(actions );
   //  require('./features/versioning.spec')(actions);
    require('./features/environmentWorkflow.spec')(actions);
 });
