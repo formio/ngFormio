e527b1ea9d76b47331f3296d4336e63a1be3a56a
rakesh <rxb162130@utdallas.edu>
2017-11-13 11:30:23 -0600

    Automated tests for beta

--- test/test.js
+++ test/test.js
@@ -35,27 +35,6 @@ before(function (next) {
 });
 
 describe("Formio Tests", function () {
-<<<<<<< HEAD
-  // require('./features/register.spec')(actions);
-  // require('./features/loginFunctionality.spec')(actions);
-  // require('./features/userPortalandWelcome.spec')(actions);
-  // require('./features/feedbackrequest.spec')(actions);
-  // require('./features/documentationLinks.spec')(actions);
-  // require('./features/profileFunctionality.spec')(actions);
-  // require('./features/project.spec')(actions);
-  // require('./features/projectSettings.spec')(actions);
-  // require('./features/form.spec')(actions);
-  // require('./features/environments.spec')(actions);
-  // //require('./features/createResource2.spec')(actions);
-  // //require('./features/createForm.spec')(actions);
-  // require('./features/settingsBasic.spec')(actions);
-  // require('./features/settingsIndependent.spec')(actions);
-  // require('./features/settingsTeamPro.spec')(actions);
-  // require('./features/settingsEnterprise.spec')(actions);
-  require('./features/teams.spec')(actions);
-  // require('./features/versioning.spec')(actions);
-  // require('./features/environmentWorkflow.spec')(actions);
-=======
   this.retries(3);
   require('./features/register.spec')(actions);
   require('./features/loginFunctionality.spec')(actions);
@@ -69,7 +48,26 @@ describe("Formio Tests", function () {
   //require('./features/environments.spec')(actions);
   //require('./features/createResource2.spec')(actions);
   //require('./features/createForm.spec')(actions);
->>>>>>> 2ea013e061ade53185738c35088df7838927a05e
+  // require('./features/register.spec')(actions);
+  // require('./features/loginFunctionality.spec')(actions);
+  // require('./features/userPortalandWelcome.spec')(actions);
+  // require('./features/feedbackrequest.spec')(actions);
+  // require('./features/documentationLinks.spec')(actions);
+  // require('./features/profileFunctionality.spec')(actions);
+  // require('./features/project.spec')(actions);
+  // require('./features/projectSettings.spec')(actions);
+  // require('./features/form.spec')(actions);
+  // require('./features/environments.spec')(actions);
+  // //require('./features/createResource2.spec')(actions);
+  // //require('./features/createForm.spec')(actions);
+  require('./features/settingsBasic.spec')(actions);
+  require('./features/settingsIndependent.spec')(actions);
+  require('./features/settingsTeamPro.spec')(actions);
+  require('./features/settingsEnterprise.spec')(actions);
+  // require('./features/teams.spec')(actions);
+  // require('./features/versioning.spec')(actions);
+  require('./features/environmentWorkflow.spec')(actions);
+
 });
 
 // afterEach(function () {
