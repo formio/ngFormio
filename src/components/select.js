37b8670ae8296d298df50093c533ff5a64d1a25b
Travis Tidwell <codertravist@gmail.com>
2017-11-25 16:25:35 -0600

    Revert "    BUGFIX:Data binding Issue in edit mode #203"

--- src/components/select.js
+++ src/components/select.js
@@ -544,7 +544,7 @@ module.exports = function(app) {
                     }
 
                     return promise.then(function(data) {
-                      if (data.length > 0) {
+                      if (data) {
                         // If the selectValue prop is defined, use it.
                         if (selectValues) {
                           setResult(_get(data, selectValues, []));
