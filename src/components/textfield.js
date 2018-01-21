2c7bc7f157e00fd376c4b1de036761a9e7d0bb96
James R. Maxfield <Vercjames@gmail.com>
2018-01-21 14:53:59 -0600

    Spellcheck feature

--- src/components/textfield.js
+++ src/components/textfield.js
@@ -25,6 +25,7 @@ module.exports = function(app) {
           persistent: true,
           hidden: false,
           clearOnHide: true,
+          spellcheck: true,
           validate: {
             required: false,
             minLength: '',
