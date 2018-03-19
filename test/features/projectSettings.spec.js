3fa39bec93f314af111655bf4dff53c47d35e68c
rakesh <rxb162130@utdallas.edu>
2018-03-19 16:46:23 -0500

    Updating chrome on circleCI

--- test/features/projectSettings.spec.js
+++ test/features/projectSettings.spec.js
@@ -1,18 +1,15 @@
 module.exports = function (actions) {
   describe('Project Update Setting Functionality', function () {
     describe('Project Settings', function () {
-
       var val = [
         ['Test Project', '#title', '.project.well>h4>a', 'Test Project'],
-        ['Test Description', '#description', '.project-description', '${project3.title}']
+        ['Test Description', '#description', '.project-description', 'Test Description']
       ];
-
       actions.logout();
       actions.iAmLoggedInFor('projuser2');
       actions.goToPage("#/");
       actions.projectExisting('${random-title>project3.title}', '${random-description>project3.description}');
       actions.iSeeTextIn('.project.well>h4>a', '${project3.title}');
-
       val.forEach(function (settings) {
         actions.clickOnButton('Manage');
         actions.portalIamOn('${project3.title}');
@@ -21,7 +18,7 @@ module.exports = function (actions) {
         actions.clickOnButton('Save Project');
         actions.portalIamOn('${project3.title}');
         actions.goToPage("#/");
-        actions.iSeeValueIn(settings[2], settings[3]);
+        actions.iSeeTextIn(settings[2], settings[3]);
       });
     });
   });
