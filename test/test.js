159bf8f10df1d6d23c578ff66cd41faf4b576b80
rakesh <rxb162130@utdallas.edu>
2018-03-19 23:31:01 -0500

    Tests

--- test/test.js
+++ test/test.js
@@ -41,7 +41,7 @@ describe("Formio Tests", function () {
   require('./features/project.spec')(actions);
   // require('./features/projectSettings.spec')(actions);
   require('./features/form.spec')(actions);
-  require('./features/environments.spec')(actions);
+  // require('./features/environments.spec')(actions);
   // require('./features/createResource2.spec')(actions);
   // require('./features/createForm.spec')(actions);
   // require('./features/settingsBasic.spec')(actions);
