1703bc85f8743db50e19d74cb700a38d62e9ac1a
Travis Tidwell <travis@form.io>
2018-05-03 16:51:45 -0500

    Fixing the form builder.

--- src/directives/formBuilder.js
+++ src/directives/formBuilder.js
@@ -1,17 +1,10 @@
 import { Formio } from 'formiojs/lib/formio.builder';
-import _ from 'lodash';
-const app = angular.module('formio');
-export default app.directive('formBuilder', function() {
+export default angular.module('formio').directive('formBuilder', function() {
   return {
     restrict: 'E',
     replace: true,
     scope: {
       form: '=?',
-      src: '=',
-      url: '=?',
-      type: '=',
-      onSave: '=',
-      onCancel: '=',
       options: '<'
     },
     link: function (scope, element) {
@@ -20,40 +13,32 @@ export default app.directive('formBuilder', function() {
     controller: [
       '$scope',
       function ($scope) {
+        let builder = null;
         let builderReady = null;
         let builderElement = null;
-        $scope.onSave = $scope.onSave || _.noop;
 
-        const createBuilder = function(type) {
-          if (type && $scope.form) {
-            $scope.form.type = type;
-          }
-          builderElement.innerHTML = '';
-          builderReady = Formio.builder(builderElement, $scope.form).then((builder) => {
-            builder.on('saveComponent', () => $scope.onSave(builder.schema));
-            builder.on('deleteComponent', () => $scope.onSave(builder.schema));
-            builder.on('cancelComponent', () => $scope.onSave(builder.schema));
-            return builder;
-          });
-        };
-
-        const setBuilderProperty = function(prop, value) {
-          if (builderReady) {
-            builderReady.then((builder) => {
-              builder[prop] = value;
-            });
-          }
-        };
-
+        // Initialize the builder.
         $scope.initBuilder = function(element) {
           builderElement = element;
-          createBuilder();
+          builderElement.innerHTML = '';
+          builder = new Formio.Builder(builderElement, $scope.form);
+          builderReady = builder.setDisplay($scope.form.display);
         };
 
-        $scope.$watch('src', src => setBuilderProperty('setSrc', src));
-        $scope.$watch('url', url => setBuilderProperty('setUrl', url));
+        // Detect when the display changes.
+        $scope.$watch('form.display', display => {
+          if (builderReady && display) {
+            builderReady.then(() => builder.setDisplay(display));
+          }
+        });
+
+        $scope.$on('$destroy', function () {
+          if (builder && builder.instance) {
+            builder.instance.destroy(true);
+          }
+        });
       }
     ],
-    template: '<div />'
+    template: '<div/>'
   };
 });
