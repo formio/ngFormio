006d9185a66b1abd3c0195e47f1fbe222ff99f5b
Randall Knutson <randall@form.io>
2018-05-25 11:16:35 -0500

    Remove project upgrade tests.

--- test/test.js
+++ test/test.js
@@ -50,7 +50,7 @@ describe("Formio Tests", function () {
   require('./features/settingsTeamPro.spec')(actions);
   require('./features/settingsEnterprise.spec')(actions);
   // require('./features/teams.spec')(actions);
-  require('./features/projectUpgradePayment.spec')(actions);
+  // require('./features/projectUpgradePayment.spec')(actions);
   // require('./features/versioning.spec')(actions);
   require('./features/environmentWorkflow.spec')(actions);
 
