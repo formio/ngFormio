8694f436ec18f8a715068c65df37def19bcdc80c
Travis Tidwell <travis@form.io>
2020-02-10 11:28:39 -0600

    Updated build and version.

--- dist/ng-formio.js
+++ dist/ng-formio.js
@@ -594,7 +594,7 @@ exports.default = void 0;
 
 var _formiojs = __webpack_require__(/*! formiojs */ "formiojs");
 
-function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
 
 var app = angular.module('formio');
 
