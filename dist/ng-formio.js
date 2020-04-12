2d00a251006b1749b25ea0ad9f728315993703f0
Travis Tidwell <travis@form.io>
2020-04-12 18:49:14 -0500

    Updated build and version.

--- dist/ng-formio.js
+++ dist/ng-formio.js
@@ -3620,7 +3620,7 @@ var store = __webpack_require__(/*! ../internals/shared-store */ "./node_modules
 (module.exports = function (key, value) {
   return store[key] || (store[key] = value !== undefined ? value : {});
 })('versions', []).push({
-  version: '3.6.4',
+  version: '3.6.5',
   mode: IS_PURE ? 'pure' : 'global',
   copyright: '© 2020 Denis Pushkarev (zloirock.ru)'
 });
