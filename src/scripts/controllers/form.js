9f6fa57d0a8976c29a84b6bc9449e2725b431b8f
Tanya Gashtold <tanya@form.io>
2020-05-13 15:29:01 +0300

    fixed always save minified form schema

--- src/scripts/controllers/form.js
+++ src/scripts/controllers/form.js
@@ -975,6 +975,28 @@ app.controller('FormController', [
         .catch(FormioAlerts.onError.bind(FormioAlerts));
     };
 
+    const createDefinitionSchema = (form) => {
+      $scope.definition.schema = {
+        ...$scope.form,
+        components: form.components,
+      };
+
+      if(!$scope.$$phase) {
+        $scope.$apply();
+      }
+    }
+    
+    $scope.$on('formChange', (event, form) => {
+      createDefinitionSchema(form);
+    });
+
+    $scope.$on('formio.render', (event, form) => {
+      //set definition schema on load and change definition schema if display mode is changed
+      if (form.components && (!$scope.definition.schema ||$scope.definition.schema.display !== form.display)) {
+        createDefinitionSchema(form);
+      }
+    });
+
     // Called when the form is updated.
     $scope.$on('formUpdate', function(event, form) {
       event.stopPropagation();
@@ -988,7 +1010,6 @@ app.controller('FormController', [
       $scope.updateCurrentFormResources(form);
       $scope.form = form;
     });
-
 
     $rootScope.currentForm = $scope.form;
   }
@@ -1120,14 +1141,7 @@ app.controller('FormEditController', [
     $scope.changes = [];
 
     $scope.$on('formChange', (event, form) => {
-      $scope.definition.schema = {
-        ...$scope.form,
-        components: form.components,
-      };
       $scope.dirty = true;
-      if(!$scope.$$phase) {
-        $scope.$apply();
-      }
     });
 
     $scope.$on('formio.addComponent', (event, component, parent, path, index) => {
